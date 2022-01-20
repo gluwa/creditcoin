e1604623e014bf00fc35aadc5bd5ee8df361204e
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-01-20 17:37:36 -0500

    creditcoin: basic benchmarking

--- pallets/creditcoin/src/benchmarking.rs
+++ pallets/creditcoin/src/benchmarking.rs
@@ -3,18 +3,32 @@
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as Template;
+use crate::Pallet as Creditcoin;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
-
-benchmarks! {
-	do_something {
-		let s in 0 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
-	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
+#[extend::ext]
+impl<'a, S> &'a [u8]
+where
+	S: Get<u32>,
+{
+	fn try_into_bounded(self) -> Result<BoundedVec<u8, S>, ()> {
+		core::convert::TryFrom::try_from(self.to_vec())
+	}
+	fn into_bounded(self) -> BoundedVec<u8, S> {
+		core::convert::TryFrom::try_from(self.to_vec()).unwrap()
 	}
 }
 
-impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
+benchmarks! {
+	register_address {
+		let caller: T::AccountId = whitelisted_caller();
+		let b in 0..256;
+		let e in 0..256;
+		let n in 0..256;
+		let blockchain = vec![b'b'; b as usize];
+		let external_address = vec![b'a'; e as usize];
+		let network = vec![b'c'; n as usize];
+	}: _(RawOrigin::Signed(caller), blockchain.into_bounded(), external_address.into_bounded(), network.into_bounded())
+}
+
+impl_benchmark_test_suite!(Creditcoin, crate::mock::new_test_ext(), crate::mock::Test);
