d6ac440833eee0c9bd84a39735b60fa223770610
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-01-24 19:06:22 -0500

    Update dockerfile and entrypoint

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,13 @@ RUN apt-get update && apt-get install -y \
 RUN curl https://sh.rustup.rs -sSf | sh -s -- -y
 RUN source ~/.cargo/env && rustup default stable && rustup update nightly && rustup update stable && rustup target add wasm32-unknown-unknown --toolchain nightly
 WORKDIR /creditcoin-node
-COPY . /creditcoin-node
+COPY Cargo.toml .
+COPY Cargo.lock .
+ADD node /creditcoin-node/node
+ADD pallets /creditcoin-node/pallets
+ADD primitives /creditcoin-node/primitives
+ADD runtime /creditcoin-node/runtime
+ADD sha3pow /creditcoin-node/sha3pow
 RUN source ~/.cargo/env && cargo build --release
 
 FROM ubuntu:latest
@@ -23,7 +29,7 @@ EXPOSE 30333/udp
 EXPOSE 9944
 EXPOSE 9933
 COPY --from=builder /creditcoin-node/target/release/creditcoin-node /bin/creditcoin-node
-COPY --from=builder /creditcoin-node/testnetSpec.json /chainspec/testnetSpec.json
+COPY testnetSpec.json /chainspec/testnetSpec.json
 COPY entrypoint.sh .
-RUN chmod +x entrypoint.sh
-ENTRYPOINT [ "entrypoint.sh" ]
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT [ "/bin/bash", "-c", "./entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,19 @@
-#!/bin/env bash
+#!/bin/bash
 
-if [[ -n "$BOOTNODE" ]]; then
-    /bin/creditcoin-node --chain /chainspec/testnetSpec.json --node-key "$NODE_KEY " --name "$NODE_NAME" --bootnodes "$BOOTNODE" --port 30333 --ws-port 9944 --rpc-port 9933 \
-    --base-path "$BASE_PATH"
+if [ -n "$BOOTNODE_IP" ]; then
+    boot_id="${BOOTNODE_PEER_ID:-12D3KooWSnPk7hN9epDUonVuNVgTvxbmnuQbGP8ghMvuoH9GAsz5}"
+    if [ "$NODE_NAME"]; then
+        name="--name $NODE_NAME"
+    else
+        name=""
+    fi
+    /bin/creditcoin-node --chain /chainspec/testnetSpec.json $name --bootnodes "/ip4/$BOOTNODE_IP/tcp/30333/p2p/$boot_id" \
+     --port 30333 --ws-port 9944 --rpc-port 9933 --validator \
+     --telemetry-url "wss://telemetry.polkadot.io/submit/ 0"
 else
-    /bin/creditcoin-node --chain /chainspec/testnetSpec.json --node-key "$NODE_KEY " --name "$NODE_NAME" --port 30333 --ws-port 9944 --rpc-port 9933 \
-    --base-path "$BASE_PATH"
+    key="${NODE_KEY:-c5eb4a9ada5c9dd76378d000f046e8cde064d68e96a1df569190eee70afba8e7}"
+    node_name="${NODE_NAME:-bootnode}"
+    /bin/creditcoin-node --chain /chainspec/testnetSpec.json --node-key $key --name "$node_name" \
+     --port 30333 --ws-port 9944 --rpc-port 9933 --validator \
+     --telemetry-url "wss://telemetry.polkadot.io/submit/ 0"
 fi
