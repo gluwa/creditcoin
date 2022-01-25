44a4bdf42af2243d5b9125cfb994f0213a48c8d0
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-01-25 16:43:22 -0500

    Add configurable FQDN to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,13 @@ if [ -n "$BOOTNODE_IP" ]; then
     fi
     /bin/creditcoin-node --chain /chainspec/testnetSpec.json $name --bootnodes "/ip4/$BOOTNODE_IP/tcp/30333/p2p/$boot_id" \
      --port 30333 --ws-port 9944 --rpc-port 9933 --validator \
+     --public-addr "/dns4/$FQDN/tcp/30333" \
      --telemetry-url "wss://telemetry.polkadot.io/submit/ 0"
 else
     key="${NODE_KEY:-c5eb4a9ada5c9dd76378d000f046e8cde064d68e96a1df569190eee70afba8e7}"
     node_name="${NODE_NAME:-bootnode}"
     /bin/creditcoin-node --chain /chainspec/testnetSpec.json --node-key $key --name "$node_name" \
      --port 30333 --ws-port 9944 --rpc-port 9933 --validator \
+     --public-addr "/dns4/$FQDN/tcp/30333" \
      --telemetry-url "wss://telemetry.polkadot.io/submit/ 0"
 fi
