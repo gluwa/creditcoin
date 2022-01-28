50abf5dd66b3b8211e045adc0f7fc55287cb0a74
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-01-28 11:57:43 -0500

    Pass accountid instead of public key

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -43,6 +43,8 @@ pub type BlockNumber = u32;
 /// Alias to 512-bit hash when used in the context of a transaction signature on the chain.
 pub type Signature = MultiSignature;
 
+pub type Signer = <Signature as Verify>::Signer;
+
 /// Some way of identifying an account on the chain. We intentionally make it equivalent
 /// to the public key of our transaction signing scheme.
 pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
