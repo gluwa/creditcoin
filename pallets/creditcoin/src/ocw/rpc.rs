3715555626a09aa5bc081d72d2106aa88b1a374c
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-02-01 15:37:42 -0500

    creditcoin: make RPCs to ethereum nodes

---
+++ pallets/creditcoin/src/ocw/rpc.rs
@@ -0,0 +1,272 @@
+use alloc::string::String;
+use core::{convert::TryFrom, fmt};
+use ethereum_types::{H160, H256, U256, U64};
+use serde::{
+	de::{Error, Unexpected, Visitor},
+	Deserialize, Deserializer, Serialize, Serializer,
+};
+use sp_runtime::offchain::{http, Duration, Timestamp};
+use sp_std::{prelude::*, vec::Vec};
+
+use crate::ExternalTxId;
+
+#[repr(transparent)]
+#[derive(Clone, Debug)]
+pub struct VecString(Vec<u8>);
+
+impl TryFrom<&[u8]> for VecString {
+	type Error = core::str::Utf8Error;
+	fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+		let _ = core::str::from_utf8(value)?;
+		Ok(VecString(Vec::from(value)))
+	}
+}
+
+impl From<&str> for VecString {
+	fn from(s: &str) -> Self {
+		VecString(Vec::from(s.as_bytes()))
+	}
+}
+
+impl serde::Serialize for VecString {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: serde::Serializer,
+	{
+		serde::Serialize::serialize(core::str::from_utf8(&self.0).unwrap(), serializer)
+	}
+}
+
+impl<'de> serde::Deserialize<'de> for VecString {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: serde::Deserializer<'de>,
+	{
+		let s: &str = serde::Deserialize::deserialize(deserializer)?;
+		Ok(VecString(Vec::from(s.as_bytes())))
+	}
+}
+
+/// Raw bytes wrapper
+#[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
+pub struct Bytes(pub Vec<u8>);
+
+impl<T: Into<Vec<u8>>> From<T> for Bytes {
+	fn from(data: T) -> Self {
+		Bytes(data.into())
+	}
+}
+
+impl Serialize for Bytes {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: Serializer,
+	{
+		let mut serialized = String::from("0x");
+		serialized.push_str(&hex::encode(&self.0));
+		serializer.serialize_str(serialized.as_ref())
+	}
+}
+
+impl<'a> Deserialize<'a> for Bytes {
+	fn deserialize<D>(deserializer: D) -> Result<Bytes, D::Error>
+	where
+		D: Deserializer<'a>,
+	{
+		deserializer.deserialize_identifier(BytesVisitor)
+	}
+}
+
+pub type Address = H160;
+pub type Index = U64;
+
+struct BytesVisitor;
+
+impl<'a> Visitor<'a> for BytesVisitor {
+	type Value = Bytes;
+
+	fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+		write!(formatter, "a 0x-prefixed hex-encoded vector of bytes")
+	}
+
+	fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+	where
+		E: Error,
+	{
+		if value.len() >= 2 && &value[0..2] == "0x" {
+			let bytes = hex::decode(&value[2..])
+				.map_err(|e| Error::custom(alloc::format!("Invalid hex: {}", e)))?;
+			Ok(Bytes(bytes))
+		} else {
+			Err(Error::invalid_value(Unexpected::Str(value), &"0x prefix"))
+		}
+	}
+
+	fn visit_string<E>(self, value: String) -> Result<Self::Value, E>
+	where
+		E: Error,
+	{
+		self.visit_str(value.as_ref())
+	}
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
+pub struct JsonRpcRequest {
+	jsonrpc: VecString,
+	method: VecString,
+	params: Vec<serde_json::Value>,
+	id: u64,
+}
+
+const REQUEST_TIMEOUT_PERIOD: Duration = Duration::from_millis(5000);
+
+fn timeout() -> Timestamp {
+	sp_io::offchain::timestamp().add(REQUEST_TIMEOUT_PERIOD)
+}
+
+impl JsonRpcRequest {
+	pub fn with_method(method: impl Into<VecString>) -> Self {
+		let method = method.into();
+		Self { jsonrpc: VecString::from("2.0"), method, params: Vec::new(), id: 1 }
+	}
+	pub fn new(
+		method: impl Into<VecString>,
+		params: impl IntoIterator<Item = serde_json::Value>,
+	) -> Self {
+		Self {
+			jsonrpc: VecString::from("2.0"),
+			method: method.into(),
+			params: params.into_iter().collect(),
+			id: 1,
+		}
+	}
+	pub fn param(&mut self, param: serde_json::Value) -> &mut Self {
+		self.params.push(param);
+		self
+	}
+	pub fn to_bytes(&self) -> Vec<u8> {
+		serde_json::to_vec(self).expect("serialization cannot fail; qed")
+	}
+	pub fn send<T: for<'de> serde::Deserialize<'de>>(self, rpc_url: &str) -> Result<T, ()> {
+		let rpc_bytes = serde_json::to_vec(&self).map_err(|e| {
+			log::error!("rpc error: {}", e);
+			()
+		})?;
+		let timeout = timeout();
+		let response = http::Request::post(rpc_url, vec![rpc_bytes])
+			.add_header("Content-Type", "application/json")
+			.send()
+			.map_err(|err| {
+				log::error!("Received http error: {:?}", err);
+				()
+			})?
+			.try_wait(timeout)
+			.unwrap()
+			.unwrap();
+		let body: Vec<u8> = response.body().collect();
+		let rpc_response: JsonRpcResponse<T> = serde_json::from_slice(&body).unwrap();
+		Ok(rpc_response.result().unwrap())
+	}
+}
+
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct JsonRpcResponse<T> {
+	jsonrpc: VecString,
+	id: u64,
+	error: Option<JsonRpcError>,
+	result: Option<T>,
+}
+
+impl<T> JsonRpcResponse<T> {
+	pub fn result(self) -> Result<T, JsonRpcError> {
+		if let Some(err) = self.error {
+			return Err(err)
+		}
+		if let Some(result) = self.result {
+			Ok(result)
+		} else {
+			unreachable!("neither an error nor result found");
+		}
+	}
+}
+
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct JsonRpcError {
+	code: i32,
+	message: String,
+}
+
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct EthTransaction {
+	/// Hash
+	pub hash: H256,
+	/// Nonce
+	pub nonce: U256,
+	/// Block hash. None when pending.
+	#[serde(rename = "blockHash")]
+	pub block_hash: Option<H256>,
+	/// Block number. None when pending.
+	#[serde(rename = "blockNumber")]
+	pub block_number: Option<U64>,
+	/// Transaction Index. None when pending.
+	#[serde(rename = "transactionIndex")]
+	pub transaction_index: Option<Index>,
+	/// Sender
+	#[serde(default, skip_serializing_if = "Option::is_none")]
+	pub from: Option<Address>,
+	/// Recipient (None when contract creation)
+	pub to: Option<Address>,
+	/// Transfered value
+	pub value: U256,
+	/// Input data
+	pub input: Bytes,
+}
+
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct EthTransactionReceipt {
+	/// Transaction hash.
+	#[serde(rename = "transactionHash")]
+	pub transaction_hash: H256,
+	/// Index within the block.
+	#[serde(rename = "transactionIndex")]
+	pub transaction_index: Index,
+	/// Hash of the block this transaction was included within.
+	#[serde(rename = "blockHash")]
+	pub block_hash: Option<H256>,
+	/// Number of the block this transaction was included within.
+	#[serde(rename = "blockNumber")]
+	pub block_number: Option<U64>,
+	/// Sender
+	/// Note: default address if the client did not return this value
+	/// (maintains backwards compatibility for <= 0.7.0 when this field was missing)
+	#[serde(default)]
+	pub from: Address,
+	/// Recipient (None when contract creation)
+	/// Note: Also `None` if the client did not return this value
+	/// (maintains backwards compatibility for <= 0.7.0 when this field was missing)
+	#[serde(default)]
+	pub to: Option<Address>,
+	/// Status: either 1 (success) or 0 (failure).
+	pub status: Option<U64>,
+}
+
+pub fn get_eth_transaction(tx_id: &ExternalTxId, rpc_url: &str) -> Result<EthTransaction, ()> {
+	let rpc_req = JsonRpcRequest::new(
+		"eth_getTransactionByHash",
+		vec![serde_json::Value::String(
+			alloc::string::String::from_utf8(tx_id.clone().into()).map_err(|_| ())?,
+		)],
+	);
+	rpc_req.send(rpc_url)
+}
+
+pub fn get_eth_transaction_receipt(
+	tx_id: &ExternalTxId,
+	rpc_url: &str,
+) -> Result<EthTransactionReceipt, ()> {
+	let rpc_req = JsonRpcRequest::new(
+		"eth_getTransactionReceipt",
+		vec![serde_json::Value::String(String::from_utf8(tx_id.clone().into()).map_err(|_| ())?)],
+	);
+	rpc_req.send(rpc_url)
+}
