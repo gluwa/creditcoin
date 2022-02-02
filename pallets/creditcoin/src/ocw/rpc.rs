ee6de49b595a052c96d8e8240645a86bb6e5ee85
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-02-02 14:05:46 -0500

    creditcoin: proper error handling for ocw validation

--- pallets/creditcoin/src/ocw/rpc.rs
+++ pallets/creditcoin/src/ocw/rpc.rs
@@ -1,3 +1,6 @@
+use self::errors::RpcError;
+
+use super::OffchainResult;
 use alloc::string::String;
 use core::{convert::TryFrom, fmt};
 use ethereum_types::{H160, H256, U256, U64};
@@ -10,21 +13,45 @@ use sp_std::{prelude::*, vec::Vec};
 
 use crate::ExternalTxId;
 
+pub mod errors {
+	use super::JsonRpcError;
+	use crate::ocw::errors::impl_from_error;
+	use sp_runtime::offchain::HttpError;
+
+	#[derive(Debug)]
+	pub enum RpcError {
+		NoResult,
+		FailureResponse(JsonRpcError),
+		SerdeError(serde_json::Error),
+		HttpError(HttpError),
+		RequestError(sp_runtime::offchain::http::Error),
+		InvalidArgument(&'static str),
+		Timeout,
+	}
+
+	impl_from_error!(
+		RpcError,
+		JsonRpcError => FailureResponse,
+		serde_json::Error => SerdeError,
+		HttpError => HttpError
+	);
+}
+
 #[repr(transparent)]
 #[derive(Clone, Debug)]
-pub struct VecString(Vec<u8>);
+pub struct VecString(Vec<u8>, ());
 
 impl TryFrom<&[u8]> for VecString {
 	type Error = core::str::Utf8Error;
 	fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
 		let _ = core::str::from_utf8(value)?;
-		Ok(VecString(Vec::from(value)))
+		Ok(VecString(Vec::from(value), ()))
 	}
 }
 
 impl From<&str> for VecString {
 	fn from(s: &str) -> Self {
-		VecString(Vec::from(s.as_bytes()))
+		VecString(Vec::from(s.as_bytes()), ())
 	}
 }
 
@@ -33,7 +60,11 @@ impl serde::Serialize for VecString {
 	where
 		S: serde::Serializer,
 	{
-		serde::Serialize::serialize(core::str::from_utf8(&self.0).unwrap(), serializer)
+		serde::Serialize::serialize(
+			core::str::from_utf8(&self.0)
+				.expect("vecstrings cannot be constructed without validating utf8; qed"),
+			serializer,
+		)
 	}
 }
 
@@ -43,7 +74,7 @@ impl<'de> serde::Deserialize<'de> for VecString {
 		D: serde::Deserializer<'de>,
 	{
 		let s: &str = serde::Deserialize::deserialize(deserializer)?;
-		Ok(VecString(Vec::from(s.as_bytes())))
+		Ok(VecString(Vec::from(s.as_bytes()), ()))
 	}
 }
 
@@ -147,25 +178,24 @@ impl JsonRpcRequest {
 	pub fn to_bytes(&self) -> Vec<u8> {
 		serde_json::to_vec(self).expect("serialization cannot fail; qed")
 	}
-	pub fn send<T: for<'de> serde::Deserialize<'de>>(self, rpc_url: &str) -> Result<T, ()> {
-		let rpc_bytes = serde_json::to_vec(&self).map_err(|e| {
-			log::error!("rpc error: {}", e);
-			()
-		})?;
+	pub fn send<T: for<'de> serde::Deserialize<'de>>(
+		self,
+		rpc_url: &str,
+	) -> OffchainResult<T, RpcError> {
+		let rpc_bytes = serde_json::to_vec(&self).map_err(RpcError::SerdeError)?;
 		let timeout = timeout();
 		let response = http::Request::post(rpc_url, vec![rpc_bytes])
 			.add_header("Content-Type", "application/json")
-			.send()
-			.map_err(|err| {
-				log::error!("Received http error: {:?}", err);
-				()
-			})?
+			.send()?
 			.try_wait(timeout)
-			.unwrap()
-			.unwrap();
+			.map_err(|e| {
+				log::warn!("rpc timed out: {:?}", e);
+				RpcError::Timeout
+			})?
+			.map_err(|e| RpcError::RequestError(e))?;
 		let body: Vec<u8> = response.body().collect();
-		let rpc_response: JsonRpcResponse<T> = serde_json::from_slice(&body).unwrap();
-		Ok(rpc_response.result().unwrap())
+		let rpc_response: JsonRpcResponse<T> = serde_json::from_slice(&body)?;
+		rpc_response.result()
 	}
 }
 
@@ -180,14 +210,14 @@ pub struct JsonRpcResponse<T> {
 }
 
 impl<T> JsonRpcResponse<T> {
-	pub fn result(self) -> Result<T, JsonRpcError> {
+	pub fn result(self) -> Result<T, RpcError> {
 		if let Some(err) = self.error {
-			return Err(err)
+			return Err(err.into())
 		}
 		if let Some(result) = self.result {
 			Ok(result)
 		} else {
-			unreachable!("neither an error nor result found");
+			Err(RpcError::NoResult)
 		}
 	}
 }
@@ -263,11 +293,17 @@ impl EthTransactionReceipt {
 	}
 }
 
-pub fn eth_get_transaction(tx_id: &ExternalTxId, rpc_url: &str) -> Result<EthTransaction, ()> {
+pub fn eth_get_transaction(
+	tx_id: &ExternalTxId,
+	rpc_url: &str,
+) -> OffchainResult<EthTransaction, RpcError> {
 	let rpc_req = JsonRpcRequest::new(
 		"eth_getTransactionByHash",
 		vec![serde_json::Value::String(
-			alloc::string::String::from_utf8(tx_id.clone().into()).map_err(|_| ())?,
+			alloc::string::String::from_utf8(tx_id.clone().into()).map_err(|err| {
+				log::error!("failed to get eth transaction, tx id is invalid utf8: {}", err);
+				RpcError::InvalidArgument("transaction id is invalid utf8")
+			})?,
 		)],
 	);
 	rpc_req.send(rpc_url)
@@ -276,15 +312,23 @@ pub fn eth_get_transaction(tx_id: &ExternalTxId, rpc_url: &str) -> Result<EthTra
 pub fn eth_get_transaction_receipt(
 	tx_id: &ExternalTxId,
 	rpc_url: &str,
-) -> Result<EthTransactionReceipt, ()> {
+) -> OffchainResult<EthTransactionReceipt, RpcError> {
 	let rpc_req = JsonRpcRequest::new(
 		"eth_getTransactionReceipt",
-		vec![serde_json::Value::String(String::from_utf8(tx_id.clone().into()).map_err(|_| ())?)],
+		vec![serde_json::Value::String(String::from_utf8(tx_id.clone().into()).map_err(
+			|err| {
+				log::error!(
+					"failed to get eth transaction receipt, tx id is invalid utf8: {}",
+					err
+				);
+				RpcError::InvalidArgument("transaction id is invalid utf8")
+			},
+		)?)],
 	);
 	rpc_req.send(rpc_url)
 }
 
-pub fn eth_get_block_number(rpc_url: &str) -> Result<U64, ()> {
+pub fn eth_get_block_number(rpc_url: &str) -> OffchainResult<U64, RpcError> {
 	let rpc_req = JsonRpcRequest::new("eth_blockNumber", vec![]);
 	rpc_req.send(rpc_url)
 }
