c0993dd8bcc141fa39ac5d20c3fe6d01602e6ef5
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-02-17 17:54:28 -0500

    add spec for QA network, support authority nodes, switch networks in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
+#shellcheck disable=SC2153
 
 base_path="--base-path ${DATA:-/data}"
-chain="--chain ${CHAIN:-/testnetSpec.json}"
+
+if [ "$CHAIN" = "testnet" ]; then
+    chain="--chain /testnetSpec.json"
+elif [ "$CHAIN" = "qa" ]; then
+    chain="--chain /qaSpec.json"
+elif [ -z "$CHAIN" ]; then # no chain specified, fallback to testnet for now
+    chain="--chain /testnetSpec.json"
+else # try whatever is specified
+    chain="--chain ${CHAIN}"
+fi
+
 if [ "$MODE" = "rpc" ]; then
     ws='--ws-external'
     rpc='--rpc-external'
     cors="--rpc-cors ${CORS:-all}"
     pruning="--pruning ${PRUNING:-archive}"
 else
+    validator='--validator'
     if [ "$MINING_KEY" ]; then
         mining_key="--mining_key ${MINING_KEY}"
     else
         echo 'Generating mining keypair because none was specified, access the keystore to see the secret key'
+        #shellcheck disable=SC2086
         mining_key="--mining-key $(/bin/creditcoin-node generate-mining-key $chain --quiet $base_path)"
     fi
-    validator='--validator'
+    if [ "$MODE" = "authority" ]; then
+        if [ -n "$AUTHORITY_SECRET" ]; then
+            #shellcheck disable=SC2086
+            /bin/creditcoin-node key insert $chain $base_path --key-type 'ctcs' --scheme 'Sr25519' --suri "$AUTHORITY_SECRET" || exit 1
+        else
+            echo 'Authority nodes require AUTHORITY_SECRET env var'
+            exit 1
+        fi
+    fi
 fi
 if [ "$RESYNC" ]; then
+    #shellcheck disable=SC2086
     /bin/creditcoin-node purge-chain -y $chain $base_path
 fi
 if [ "$PROMETHEUS" = "1" ]; then
@@ -32,6 +54,7 @@ if [ -n "$BOOTNODE_IP" ] || [ -n "$BOOTNODE_FQDN" ]; then
     else
         bootnode="--bootnodes /ip4/$BOOTNODE_IP/tcp/30333/p2p/$boot_id"
     fi
+    #shellcheck disable=SC2086
     /bin/creditcoin-node $name $bootnode \
      --port 30333 --ws-port 9944 --rpc-port 9933 \
      --public-addr "/dns4/$FQDN/tcp/30333" \
@@ -41,6 +64,7 @@ if [ -n "$BOOTNODE_IP" ] || [ -n "$BOOTNODE_FQDN" ]; then
 else
     key="${NODE_KEY:-c5eb4a9ada5c9dd76378d000f046e8cde064d68e96a1df569190eee70afba8e7}"
     node_name="${NODE_NAME:-bootnode}"
+    #shellcheck disable=SC2086
     /bin/creditcoin-node --node-key "$key" --name "$node_name" \
      --port 30333 --ws-port 9944 --rpc-port 9933 \
      --public-addr "/dns4/$FQDN/tcp/30333" \
