ae9c3742bd016b0d3d6e3f44c702bc2d6d904cd5
Jeremy Frank <jeremy.frank@gluwa.com>
2022-02-21 15:52:49 -0700

    add offer_id to DealOrder

--- pallets/creditcoin/src/lib.rs
+++ pallets/creditcoin/src/lib.rs
@@ -506,7 +506,7 @@ pub mod pallet {
 			let who = ensure_signed(origin)?;
 
 			let deal_order_id = DealOrderId::new::<T>(expiration_block, &offer_id);
-			ensure!(!DealOrders::<T>::contains_id(&deal_order_id), Error::<T>::DuplicateId);
+			ensure!(!DealOrders::<T>::contains_id(&deal_order_id), Error::<T>::DuplicateDealOrder);
 
 			let offer = try_get_id!(Offers<T>, &offer_id, NonExistentOffer)?;
 
@@ -527,6 +527,7 @@ pub mod pallet {
 
 			let deal_order = DealOrder {
 				blockchain: offer.blockchain,
+				offer_id,
 				lender_address_id: ask_order.lender_address_id,
 				borrower_address_id: bid_order.borrower_address_id,
 				terms: agreed_terms,
@@ -697,6 +698,7 @@ pub mod pallet {
 
 			let deal_order = DealOrder {
 				blockchain: lender.blockchain,
+				offer_id: offer_id.clone(),
 				lender_address_id,
 				borrower_address_id,
 				terms,
