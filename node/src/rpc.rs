dce3cfc4260b550ff7c104ff5b0c6254d40c3be9
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-03-08 11:23:05 -0500

    Wire up custom RPCs

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -8,6 +8,8 @@
 use std::sync::Arc;
 
 use creditcoin_node_runtime::{opaque::Block, AccountId, Balance, Index};
+use jsonrpc_core::futures;
+use sc_client_api::BlockchainEvents;
 pub use sc_rpc_api::DenyUnsafe;
 use sc_transaction_pool_api::TransactionPool;
 use sp_api::ProvideRuntimeApi;
@@ -15,35 +17,47 @@ use sp_block_builder::BlockBuilder;
 use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
 
 /// Full client dependencies.
-pub struct FullDeps<C, P> {
+pub struct FullDeps<C, P, B, E> {
 	/// The client instance to use.
 	pub client: Arc<C>,
 	/// Transaction pool instance.
 	pub pool: Arc<P>,
-	/// Whether to deny unsafe calls
+	/// Database backend instance.
+	pub backend: Arc<B>,
+	/// Instance of an executor for spawning tasks.
+	pub executor: Arc<E>,
+	/// Whether to deny unsafe calls.
 	pub deny_unsafe: DenyUnsafe,
 }
 
 /// Instantiate all full RPC extensions.
-pub fn create_full<C, P>(deps: FullDeps<C, P>) -> jsonrpc_core::IoHandler<sc_rpc::Metadata>
+pub fn create_full<C, P, B, E>(
+	deps: FullDeps<C, P, B, E>,
+) -> jsonrpc_core::IoHandler<sc_rpc::Metadata>
 where
 	C: ProvideRuntimeApi<Block>,
-	C: HeaderBackend<Block> + HeaderMetadata<Block, Error = BlockChainError> + 'static,
+	C: HeaderBackend<Block> + HeaderMetadata<Block, Error = BlockChainError>,
+	C: BlockchainEvents<Block>,
 	C: Send + Sync + 'static,
 	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Index>,
 	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
 	C::Api: BlockBuilder<Block>,
 	P: TransactionPool + 'static,
+	B: sc_client_api::Backend<Block> + 'static,
+	E: futures::task::Spawn + Send + Sync + 'static,
 {
+	use pallet_creditcoin_rpc::{CreditcoinApi, CreditcoinRpc};
 	use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApi};
 	use substrate_frame_rpc_system::{FullSystem, SystemApi};
 
 	let mut io = jsonrpc_core::IoHandler::default();
-	let FullDeps { client, pool, deny_unsafe } = deps;
+	let FullDeps { client, pool, deny_unsafe, backend, executor } = deps;
 
 	io.extend_with(SystemApi::to_delegate(FullSystem::new(client.clone(), pool, deny_unsafe)));
 
-	io.extend_with(TransactionPaymentApi::to_delegate(TransactionPayment::new(client)));
+	io.extend_with(TransactionPaymentApi::to_delegate(TransactionPayment::new(client.clone())));
+
+	io.extend_with(CreditcoinApi::to_delegate(CreditcoinRpc::new(client, backend, executor)));
 
 	// Extend this RPC with a custom API by using the following syntax.
 	// `YourRpcStruct` should have a reference to a client, which is needed
