918ae3cd04f99c1b5404405c0edbdac69082b03e
Alex Todorov <alex.todorov@gluwa.com>
2022-04-01 14:31:58 +0000

    JavaScript formatting

--- javascript/src/utils.ts
+++ javascript/src/utils.ts
@@ -1,51 +1,61 @@
+// [object Object]
+// SPDX-License-Identifier: Apache-2.0
+
+import { randomBytes } from 'crypto';
+import { Wallet } from 'ethers';
+
 import { ApiPromise, SubmittableResult } from '@polkadot/api';
+import { PalletCreditcoinBlockchain } from '@polkadot/types/lookup';
 import { u8aConcat } from '@polkadot/util';
 import { blake2AsHex } from '@polkadot/util-crypto';
-import { PalletCreditcoinBlockchain } from '@polkadot/types/lookup';
-import { Wallet } from 'ethers';
 
 export type TxOnSuccess = (result: SubmittableResult) => void;
 export type TxOnFail = (result: SubmittableResult | Error | undefined) => void;
 
 export const handleTransaction = async (
-    api: ApiPromise,
-    unsubscribe: () => void,
-    result: SubmittableResult,
-    onSuccess: (result: SubmittableResult) => void,
-    onFail: (result: SubmittableResult | Error | undefined) => void,
+  api: ApiPromise,
+  unsubscribe: () => void,
+  result: SubmittableResult,
+  onSuccess: (result: SubmittableResult) => void,
+  onFail: (result: SubmittableResult | Error | undefined) => void
 ) => {
-    const { status, events, dispatchError } = result;
-    console.log(`current status is ${status}`);
-    if (dispatchError) {
-        if (dispatchError.isModule) {
-            const decoded = api.registry.findMetaError(dispatchError.asModule);
-            const { docs, name, section } = decoded;
+  const { dispatchError, events, status } = result;
 
-            console.log(`${section}.${name}: ${docs.join(' ')}`);
-        } else {
-            console.log(dispatchError.toString());
-        }
+  console.log(`current status is ${status}`);
 
-        onFail(result);
-        unsubscribe();
+  if (dispatchError) {
+    if (dispatchError.isModule) {
+      const decoded = api.registry.findMetaError(dispatchError.asModule);
+      const { docs, name, section } = decoded;
+
+      console.log(`${section}.${name}: ${docs.join(' ')}`);
+    } else {
+      console.log(dispatchError.toString());
     }
-    if (status.isInBlock) {
-        events.forEach(({ event }) => {
-            const types = event.typeDef;
-            event.data.forEach((data, index) => {
-                console.log(`pallet: ${event.section} event name: ${event.method}`);
-                console.log(`event types ${types[index].type} event data: ${data.toString()}`);
-            });
-        });
 
-        onSuccess(result);
-        unsubscribe();
-    }
+    onFail(result);
+    unsubscribe();
+  }
+
+  if (status.isInBlock) {
+    events.forEach(({ event }) => {
+      const types = event.typeDef;
+
+      event.data.forEach((data, index) => {
+        console.log(`pallet: ${event.section} event name: ${event.method}`);
+        console.log(`event types ${types[index].type} event data: ${data.toString()}`);
+      });
+    });
+
+    onSuccess(result);
+    unsubscribe();
+  }
 };
 
 export const getAddressId = (blockchain: PalletCreditcoinBlockchain | string, externalAddress: string) => {
-    const addressId = u8aConcat(Buffer.from(blockchain.toString().toLowerCase()), Buffer.from(externalAddress));
-    return blake2AsHex(addressId);
+  const addressId = u8aConcat(Buffer.from(blockchain.toString().toLowerCase()), Buffer.from(externalAddress));
+
+  return blake2AsHex(addressId);
 };
 
 export const randomEthAddress = () => {
