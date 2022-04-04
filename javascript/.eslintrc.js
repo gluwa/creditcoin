496d73f7e2a73c8974b8fc388ba02f14e8f805b3
Alex Todorov <alex.todorov@gluwa.com>
2022-04-04 17:09:16 +0000

    Disable some linter rules temporarily

--- javascript/.eslintrc.js
+++ javascript/.eslintrc.js
@@ -22,6 +22,14 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     // this seems very broken atm, false positives
     '@typescript-eslint/unbound-method': 'off',
+
+    // creditcoin disables
+    'no-async-promise-executor': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+
     'header/header': [2, 'line', [
       { pattern: ' Copyright 20(22|23|24)(-2022)? Gluwa, Inc.' },
       ' SPDX-License-Identifier: The Unlicense'
