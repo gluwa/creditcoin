90d9f6a96ac909b9d391698b29d6c7d2a7ff57de
Alex Todorov <alex.todorov@gluwa.com>
2022-04-04 12:58:28 +0000

    Update config for eslint

--- javascript/.eslintrc.js
+++ javascript/.eslintrc.js
@@ -6,7 +6,9 @@ const base = require('@polkadot/dev/config/eslint.cjs');
 module.exports = {
   ...base,
   ignorePatterns: [
-    ...base.ignorePatterns
+    ...base.ignorePatterns,
+    '**/src/**/*.d.ts',
+    '**/integration-tests/**/*.d.ts',
   ],
   parserOptions: {
     ...base.parserOptions,
@@ -19,6 +21,10 @@ module.exports = {
     // add override for any (a metric ton of them, initial conversion)
     '@typescript-eslint/no-explicit-any': 'off',
     // this seems very broken atm, false positives
-    '@typescript-eslint/unbound-method': 'off'
+    '@typescript-eslint/unbound-method': 'off',
+    'header/header': [2, 'line', [
+      { pattern: ' Copyright 20(22|23|24)(-2022)? Gluwa, Inc.' },
+      ' SPDX-License-Identifier: The Unlicense'
+    ], 1],
   }
 };
