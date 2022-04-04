434b33cfa046bfa454619b793431068855d2ac88
Alex Todorov <alex.todorov@gluwa.com>
2022-04-04 13:09:19 +0000

    Fix various eslint issues
    - license header & formatting
    - camelcase
    - unused references
    - sort keys
    - equals

--- javascript/src/constants.ts
+++ javascript/src/constants.ts
@@ -1,5 +1,5 @@
-// [object Object]
-// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Gluwa, Inc. & contributors
+// SPDX-License-Identifier: The Unlicense
 
 export const CREDO_PER_CTC = 1_000_000_000_000_000_000;
 export const POINT_01_CTC = 0.01 * CREDO_PER_CTC;
