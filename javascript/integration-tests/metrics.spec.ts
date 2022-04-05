2a0eaf1cdbb529e8ad9dd411284326b9ba6b0cb0
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-04-05 12:27:29 -0400

    appease js linter

--- javascript/integration-tests/metrics.spec.ts
+++ javascript/integration-tests/metrics.spec.ts
@@ -1,3 +1,6 @@
+// Copyright 2022 Gluwa, Inc. & contributors
+// SPDX-License-Identifier: The Unlicense
+
 import axios from 'axios';
 
 const METRICS_BASE = 'http://127.0.0.1:9615';
@@ -8,6 +11,9 @@ test('Hashrate prometheus metric works', async () => {
     const re = /creditcoin_node_hash_count\{chain="dev"\} (\d+)/;
     const match = data.match(re);
     expect(match).not.toBeNull();
-    const value = parseInt(match![1], 10);
-    expect(value).toBeGreaterThan(0);
+    if (match) {
+        // so TS sees the match is non-null
+        const value = parseInt(match[1], 10);
+        expect(value).toBeGreaterThan(0);
+    }
 });
