480d0d2a19f3a342f4977782607c8a76a34bf3f0
Carlos Alejandro Gutiérrez Sandoval <calgusa@gmail.com>
2022-04-04 22:04:21 -0500

    difficulty benchmarks

--- pallets/difficulty/src/benchmarking.rs
+++ pallets/difficulty/src/benchmarking.rs
@@ -1,20 +1,28 @@
+#![cfg(feature = "runtime-benchmarks")]
 //! Benchmarking setup for pallet-template
 
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as Template;
+use crate::Pallet as Difficulty;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
+use sp_arithmetic::traits::UniqueSaturatedFrom;
 
-// benchmarks! {
-// 	do_something {
-// 		let s in 0 .. 100;
-// 		let caller: T::AccountId = whitelisted_caller();
-// 	}: _(RawOrigin::Signed(caller), s)
-// 	verify {
-// 		assert_eq!(Something::<T>::get(), Some(s));
-// 	}
-// }
+benchmarks! {
+	set_target_block_time{
+	}: _(RawOrigin::Root, <T as Config>::Moment::unique_saturated_from(100u64))
+	verify {
+		assert_eq!(TargetBlockTime::<T>::get(), <T as Config>::Moment::unique_saturated_from(100u64));
+	}
 
-// impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
+	set_adjustment_period{
+		let caller: T::AccountId = whitelisted_caller();
+	}:_(RawOrigin::Root,100i64)
+	verify{
+		assert_eq!(DifficultyAdjustmentPeriod::<T>::get(),100i64);
+	}
+
+}
+
+impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
