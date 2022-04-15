11cca2e5c19e18993791b2fc850bb6c29dbb49fc
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-04-15 19:45:10 -0400

    bump runtime version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -93,10 +93,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 201,
+	spec_version: 202,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 3,
+	transaction_version: 4,
 	state_version: 1,
 };
 
