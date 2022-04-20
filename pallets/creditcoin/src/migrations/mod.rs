667efa413abeaa603d5e2b8dd1d5c902a567c3a4
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-04-20 18:52:24 -0400

    creditcoin: add interest type to `InterestRate`

--- pallets/creditcoin/src/migrations/mod.rs
+++ pallets/creditcoin/src/migrations/mod.rs
@@ -3,6 +3,7 @@ use frame_support::{traits::StorageVersion, weights::Weight};
 
 mod v1;
 mod v2;
+mod v3;
 
 pub(crate) fn migrate<T: Config>() -> Weight {
 	let version = StorageVersion::get::<Pallet<T>>();
@@ -16,6 +17,11 @@ pub(crate) fn migrate<T: Config>() -> Weight {
 	if version < 2 {
 		weight = weight.saturating_add(v2::migrate::<T>());
 		StorageVersion::new(2).put::<Pallet<T>>();
+	}
+
+	if version < 3 {
+		weight = weight.saturating_add(v3::migrate::<T>());
+		StorageVersion::new(3).put::<Pallet<T>>();
 	}
 
 	weight
