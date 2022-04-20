0968bb457d17a7ba9233e125bfaadc396c8bc10d
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-04-20 18:52:24 -0400

    creditcoin: add interest type to `InterestRate`

--- pallets/creditcoin/src/migrations/v1.rs
+++ pallets/creditcoin/src/migrations/v1.rs
@@ -1,7 +1,6 @@
-use core::convert::TryFrom;
-
 use crate::{
-	loan_terms::Duration, AddressId, Blockchain, Config, ExternalAmount, OfferId, TransferId,
+	loan_terms::{Decimals, Duration},
+	AddressId, Blockchain, Config, ExternalAmount, OfferId, RatePerPeriod, TransferId,
 };
 use codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::weights::Weight;
@@ -14,20 +13,20 @@ type OldInterestRate = u64;
 
 const OLD_INTEREST_RATE_DECIMALS: u64 = 4;
 
-#[derive(Clone, Encode, Decode, Eq, PartialEq, TypeInfo, MaxEncodedLen)]
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
 struct OldLoanTerms<Moment> {
 	amount: ExternalAmount,
 	interest_rate: OldInterestRate,
 	maturity: Moment,
 }
 
-#[derive(Clone, Encode, Decode, Eq, PartialEq, TypeInfo, MaxEncodedLen)]
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
 struct OldAskTerms<Moment>(OldLoanTerms<Moment>);
 
-#[derive(Clone, Encode, Decode, Eq, PartialEq, TypeInfo, MaxEncodedLen)]
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
 struct OldBidTerms<Moment>(OldLoanTerms<Moment>);
 
-#[derive(Clone, Encode, Decode, Eq, PartialEq, TypeInfo, MaxEncodedLen)]
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
 struct OldAskOrder<AccountId, BlockNum, Hash, Moment> {
 	blockchain: Blockchain,
 	lender_address_id: AddressId<Hash>,
@@ -37,7 +36,7 @@ struct OldAskOrder<AccountId, BlockNum, Hash, Moment> {
 	lender: AccountId,
 }
 
-#[derive(Clone, Encode, Decode, Eq, PartialEq, TypeInfo, MaxEncodedLen)]
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
 struct OldBidOrder<AccountId, BlockNum, Hash, Moment> {
 	blockchain: Blockchain,
 	borrower_address_id: AddressId<Hash>,
@@ -47,7 +46,7 @@ struct OldBidOrder<AccountId, BlockNum, Hash, Moment> {
 	borrower: AccountId,
 }
 
-#[derive(Clone, Encode, Decode, Eq, PartialEq, TypeInfo, MaxEncodedLen)]
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
 struct OldDealOrder<AccountId, BlockNum, Hash, Moment> {
 	blockchain: Blockchain,
 	offer_id: OfferId<BlockNum, Hash>,
@@ -62,13 +61,13 @@ struct OldDealOrder<AccountId, BlockNum, Hash, Moment> {
 	borrower: AccountId,
 }
 
-#[derive(Clone, Encode, Decode, Eq, PartialEq, TypeInfo, MaxEncodedLen)]
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
 pub struct DealOrder<AccountId, BlockNum, Hash, Moment> {
 	pub blockchain: Blockchain,
 	pub offer_id: OfferId<BlockNum, Hash>,
 	pub lender_address_id: AddressId<Hash>,
 	pub borrower_address_id: AddressId<Hash>,
-	pub terms: crate::LoanTerms,
+	pub terms: LoanTerms,
 	pub expiration_block: BlockNum,
 	pub timestamp: Moment,
 	pub funding_transfer_id: Option<TransferId<Hash>>,
@@ -77,61 +76,111 @@ pub struct DealOrder<AccountId, BlockNum, Hash, Moment> {
 	pub borrower: AccountId,
 }
 
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+pub struct InterestRate {
+	pub rate_per_period: RatePerPeriod,
+	pub decimals: Decimals,
+	pub period: Duration,
+}
+
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+pub struct LoanTerms {
+	pub amount: ExternalAmount,
+	pub interest_rate: InterestRate,
+	pub term_length: Duration,
+}
+
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+pub struct AskTerms(pub LoanTerms);
+
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+pub struct BidTerms(pub LoanTerms);
+
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+pub struct AskOrder<AccountId, BlockNum, Hash> {
+	pub blockchain: Blockchain,
+	pub lender_address_id: AddressId<Hash>,
+	pub terms: AskTerms,
+	pub expiration_block: BlockNum,
+	pub block: BlockNum,
+	pub lender: AccountId,
+}
+
+#[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+pub struct BidOrder<AccountId, BlockNum, Hash> {
+	pub blockchain: Blockchain,
+	pub borrower_address_id: AddressId<Hash>,
+	pub terms: BidTerms,
+	pub expiration_block: BlockNum,
+	pub block: BlockNum,
+	pub borrower: AccountId,
+}
+
 generate_storage_alias!(
 	Creditcoin,
 	DealOrders<T: Config> => DoubleMap<(Twox64Concat, T::BlockNumber), (Identity, T::Hash), DealOrder<T::AccountId, T::BlockNumber, T::Hash, T::Moment>>
 );
 
+generate_storage_alias!(
+	Creditcoin,
+	AskOrders<T: Config> => DoubleMap<(Twox64Concat, T::BlockNumber), (Identity, T::Hash), AskOrder<T::AccountId, T::BlockNumber, T::Hash>>
+);
+
+generate_storage_alias!(
+	Creditcoin,
+	BidOrders<T: Config> => DoubleMap<(Twox64Concat, T::BlockNumber), (Identity, T::Hash), BidOrder<T::AccountId, T::BlockNumber, T::Hash>>
+);
+
 pub(crate) fn migrate<T: Config>() -> Weight {
 	let mut weight: Weight = 0;
 	let weight_each = T::DbWeight::get().reads_writes(1, 1);
-	crate::AskOrders::<T>::translate::<
-		OldAskOrder<T::AccountId, T::BlockNumber, T::Hash, T::Moment>,
-		_,
-	>(|_expiration, _hash, ask| {
-		weight = weight.saturating_add(weight_each);
-		Some(crate::AskOrder {
-			block: ask.block,
-			blockchain: ask.blockchain,
-			expiration_block: ask.expiration_block,
-			lender: ask.lender,
-			lender_address_id: ask.lender_address_id,
-			terms: crate::AskTerms::try_from(crate::LoanTerms {
-				amount: ask.terms.0.amount,
-				interest_rate: crate::InterestRate {
-					rate_per_period: ask.terms.0.interest_rate,
-					decimals: OLD_INTEREST_RATE_DECIMALS,
-					period: Duration::from_millis(ask.terms.0.maturity.unique_saturated_into()),
-				},
-				term_length: Duration::from_millis(ask.terms.0.maturity.unique_saturated_into()),
+	AskOrders::<T>::translate::<OldAskOrder<T::AccountId, T::BlockNumber, T::Hash, T::Moment>, _>(
+		|_expiration, _hash, ask| {
+			weight = weight.saturating_add(weight_each);
+			Some(AskOrder {
+				block: ask.block,
+				blockchain: ask.blockchain,
+				expiration_block: ask.expiration_block,
+				lender: ask.lender,
+				lender_address_id: ask.lender_address_id,
+				terms: AskTerms(LoanTerms {
+					amount: ask.terms.0.amount,
+					interest_rate: InterestRate {
+						rate_per_period: ask.terms.0.interest_rate,
+						decimals: OLD_INTEREST_RATE_DECIMALS,
+						period: Duration::from_millis(ask.terms.0.maturity.unique_saturated_into()),
+					},
+					term_length: Duration::from_millis(
+						ask.terms.0.maturity.unique_saturated_into(),
+					),
+				}),
 			})
-			.expect("pre-existing ask orders cannot have invalid terms"),
-		})
-	});
+		},
+	);
 
-	crate::BidOrders::<T>::translate::<
-		OldBidOrder<T::AccountId, T::BlockNumber, T::Hash, T::Moment>,
-		_,
-	>(|_expiration, _hash, bid| {
-		weight = weight.saturating_add(weight_each);
-		Some(crate::BidOrder {
-			block: bid.block,
-			blockchain: bid.blockchain,
-			expiration_block: bid.expiration_block,
-			borrower: bid.borrower,
-			borrower_address_id: bid.borrower_address_id,
-			terms: crate::BidTerms::try_from(crate::LoanTerms {
-				amount: bid.terms.0.amount,
-				interest_rate: crate::InterestRate {
-					rate_per_period: bid.terms.0.interest_rate,
-					decimals: OLD_INTEREST_RATE_DECIMALS,
-					period: Duration::from_millis(bid.terms.0.maturity.unique_saturated_into()),
-				},
-				term_length: Duration::from_millis(bid.terms.0.maturity.unique_saturated_into()),
+	BidOrders::<T>::translate::<OldBidOrder<T::AccountId, T::BlockNumber, T::Hash, T::Moment>, _>(
+		|_expiration, _hash, bid| {
+			weight = weight.saturating_add(weight_each);
+			Some(BidOrder {
+				block: bid.block,
+				blockchain: bid.blockchain,
+				expiration_block: bid.expiration_block,
+				borrower: bid.borrower,
+				borrower_address_id: bid.borrower_address_id,
+				terms: BidTerms(LoanTerms {
+					amount: bid.terms.0.amount,
+					interest_rate: InterestRate {
+						rate_per_period: bid.terms.0.interest_rate,
+						decimals: OLD_INTEREST_RATE_DECIMALS,
+						period: Duration::from_millis(bid.terms.0.maturity.unique_saturated_into()),
+					},
+					term_length: Duration::from_millis(
+						bid.terms.0.maturity.unique_saturated_into(),
+					),
+				}),
 			})
-			.expect("pre-existing bid orders cannot have invalid terms"),
-		})
-	});
+		},
+	);
 
 	DealOrders::<T>::translate::<OldDealOrder<T::AccountId, T::BlockNumber, T::Hash, T::Moment>, _>(
 		|_expiration, _hash, deal| {
@@ -141,9 +190,9 @@ pub(crate) fn migrate<T: Config>() -> Weight {
 				offer_id: deal.offer_id,
 				lender_address_id: deal.lender_address_id,
 				borrower_address_id: deal.borrower_address_id,
-				terms: crate::LoanTerms {
+				terms: LoanTerms {
 					amount: deal.terms.amount,
-					interest_rate: crate::InterestRate {
+					interest_rate: InterestRate {
 						rate_per_period: deal.terms.interest_rate,
 						decimals: OLD_INTEREST_RATE_DECIMALS,
 						period: Duration::from_millis(deal.terms.maturity.unique_saturated_into()),
