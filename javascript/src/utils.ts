014524b238981a358834c0b8bbc25440e28edeab
Carlos Alejandro Gutiérrez Sandoval <calgusa@gmail.com>
2022-04-26 21:47:32 -0500

    prettier

--- javascript/src/utils.ts
+++ javascript/src/utils.ts
@@ -8,7 +8,7 @@ import { PalletCreditcoinBlockchain } from '@polkadot/types/lookup';
 import { u8aConcat } from '@polkadot/util';
 import { blake2AsHex, sha256AsU8a, blake2AsU8a } from '@polkadot/util-crypto';
 import { KeyringPair } from '@polkadot/keyring/types';
-import { joinSignature } from "@ethersproject/bytes";
+import { joinSignature } from '@ethersproject/bytes';
 
 export type TxOnSuccess = (result: SubmittableResult) => void;
 export type TxOnFail = (result: SubmittableResult | Error | undefined) => void;
@@ -63,26 +63,15 @@ export const randomEthWallet = (): Wallet => {
     return Wallet.createRandom();
 };
 
-export const ethOwnershipProof = (
-    api: ApiPromise,
-    signer: Wallet,
-    account: string
-) => {
-    return joinSignature(signer._signingKey().signDigest(ownershipProofDigest(api, account)))
-}
+export const ethOwnershipProof = (api: ApiPromise, signer: Wallet, account: string) => {
+    return joinSignature(signer._signingKey().signDigest(ownershipProofDigest(api, account)));
+};
 
-export const ownershipProof = (
-    api: ApiPromise,
-    signer: KeyringPair,
-    account: string
-) => {
-    return signer.sign(ownershipProofDigest(api, account))
-}
+export const ownershipProof = (api: ApiPromise, signer: KeyringPair, account: string) => {
+    return signer.sign(ownershipProofDigest(api, account));
+};
 
-const ownershipProofDigest = (
-    api: ApiPromise,
-    account: string
-) => {
+const ownershipProofDigest = (api: ApiPromise, account: string) => {
     const bytesParams = api.createType('AccountId32', account).toU8a();
-    return blake2AsU8a(sha256AsU8a(bytesParams))
-}
+    return blake2AsU8a(sha256AsU8a(bytesParams));
+};
