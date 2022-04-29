ddd7dcfe5448ffa0e1760f204de553622ea70bd2
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-04-29 18:32:27 -0400

    Add migration unit tests

--- pallets/creditcoin/src/migrations/v1.rs
+++ pallets/creditcoin/src/migrations/v1.rs
@@ -1,3 +1,6 @@
+// First `LoanTerms` rework. `maturity` is replaced with `term_length`,
+// and `InterestRate` changed from a type alias = u64 to a new struct `InterestRate`
+
 use crate::{
 	loan_terms::{Decimals, Duration},
 	AddressId, Blockchain, Config, ExternalAmount, OfferId, RatePerPeriod, TransferId,
@@ -62,6 +65,7 @@ struct OldDealOrder<AccountId, BlockNum, Hash, Moment> {
 }
 
 #[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 pub struct DealOrder<AccountId, BlockNum, Hash, Moment> {
 	pub blockchain: Blockchain,
 	pub offer_id: OfferId<BlockNum, Hash>,
@@ -77,6 +81,7 @@ pub struct DealOrder<AccountId, BlockNum, Hash, Moment> {
 }
 
 #[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 pub struct InterestRate {
 	pub rate_per_period: RatePerPeriod,
 	pub decimals: Decimals,
@@ -84,6 +89,7 @@ pub struct InterestRate {
 }
 
 #[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 pub struct LoanTerms {
 	pub amount: ExternalAmount,
 	pub interest_rate: InterestRate,
@@ -91,12 +97,15 @@ pub struct LoanTerms {
 }
 
 #[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 pub struct AskTerms(pub LoanTerms);
 
 #[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 pub struct BidTerms(pub LoanTerms);
 
 #[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 pub struct AskOrder<AccountId, BlockNum, Hash> {
 	pub blockchain: Blockchain,
 	pub lender_address_id: AddressId<Hash>,
@@ -107,6 +116,7 @@ pub struct AskOrder<AccountId, BlockNum, Hash> {
 }
 
 #[derive(Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 pub struct BidOrder<AccountId, BlockNum, Hash> {
 	pub blockchain: Blockchain,
 	pub borrower_address_id: AddressId<Hash>,
@@ -211,4 +221,192 @@ pub(crate) fn migrate<T: Config>() -> Weight {
 		},
 	);
 	weight
+}
+
+#[cfg(test)]
+mod tests {
+	use sp_core::U256;
+
+	use crate::{
+		mock::{ExtBuilder, Test},
+		tests::TestInfo,
+		AskOrderId, BidOrderId, DealOrderId, DoubleMapExt, OfferId,
+	};
+
+	use super::{
+		generate_storage_alias, AskOrder, AskTerms, BidOrder, BidTerms, Blockchain, Config,
+		Duration, Identity, InterestRate, LoanTerms, OldAskOrder, OldAskTerms, OldBidOrder,
+		OldBidTerms, OldDealOrder, OldLoanTerms, Twox64Concat, OLD_INTEREST_RATE_DECIMALS,
+	};
+
+	generate_storage_alias!(
+		Creditcoin,
+		DealOrders<T: Config> => DoubleMap<(Twox64Concat, T::BlockNumber), (Identity, T::Hash), OldDealOrder<T::AccountId, T::BlockNumber, T::Hash, T::Moment>>
+	);
+
+	type OldDealOrders = DealOrders<Test>;
+
+	generate_storage_alias!(
+		Creditcoin,
+		AskOrders<T: Config> => DoubleMap<(Twox64Concat, T::BlockNumber), (Identity, T::Hash), OldAskOrder<T::AccountId, T::BlockNumber, T::Hash, T::Moment>>
+	);
+
+	type OldAskOrders = AskOrders<Test>;
+
+	generate_storage_alias!(
+		Creditcoin,
+		BidOrders<T: Config> => DoubleMap<(Twox64Concat, T::BlockNumber), (Identity, T::Hash), OldBidOrder<T::AccountId, T::BlockNumber, T::Hash, T::Moment>>
+	);
+
+	type OldBidOrders = BidOrders<Test>;
+
+	#[test]
+	fn ask_order_migrates() {
+		ExtBuilder::default().build_and_execute(|| {
+			let ask_id = AskOrderId::new::<Test>(100, "asdf".as_bytes());
+			let test_info = TestInfo::new_defaults();
+			let old_ask = OldAskOrder {
+				blockchain: Blockchain::Ethereum,
+				lender_address_id: test_info.lender.address_id.clone(),
+				terms: OldAskTerms(OldLoanTerms {
+					amount: 1000u64.into(),
+					interest_rate: 1000,
+					maturity: 2000,
+				}),
+				expiration_block: 100,
+				block: 0,
+				lender: test_info.lender.account_id.clone(),
+			};
+			OldAskOrders::insert_id(ask_id.clone(), old_ask.clone());
+
+			super::migrate::<Test>();
+
+			let ask = super::AskOrders::<Test>::try_get_id(&ask_id).unwrap();
+
+			assert_eq!(
+				ask,
+				AskOrder {
+					blockchain: old_ask.blockchain,
+					lender_address_id: old_ask.lender_address_id,
+					terms: AskTerms(LoanTerms {
+						amount: old_ask.terms.0.amount,
+						interest_rate: InterestRate {
+							rate_per_period: old_ask.terms.0.interest_rate,
+							decimals: OLD_INTEREST_RATE_DECIMALS,
+							period: Duration::from_millis(old_ask.terms.0.maturity)
+						},
+						term_length: Duration::from_millis(old_ask.terms.0.maturity)
+					}),
+					expiration_block: old_ask.expiration_block,
+					block: old_ask.block,
+					lender: old_ask.lender,
+				}
+			)
+		});
+	}
+
+	#[test]
+	fn bid_order_migrates() {
+		ExtBuilder::default().build_and_execute(|| {
+			let bid_id = BidOrderId::new::<Test>(100, "asdf".as_bytes());
+			let test_info = TestInfo::new_defaults();
+			let address_id = test_info.borrower.address_id.clone();
+			let expiration_block = 100;
+			let block = 0;
+			let amount: U256 = 1000u64.into();
+
+			let old_bid = OldBidOrder {
+				blockchain: Blockchain::Ethereum,
+				borrower_address_id: address_id.clone(),
+				terms: OldBidTerms(OldLoanTerms {
+					amount: amount.clone(),
+					interest_rate: 1000,
+					maturity: 2000,
+				}),
+				expiration_block,
+				block,
+				borrower: test_info.borrower.account_id.clone(),
+			};
+			OldBidOrders::insert_id(bid_id.clone(), old_bid.clone());
+
+			super::migrate::<Test>();
+
+			let bid = super::BidOrders::<Test>::try_get_id(&bid_id).unwrap();
+
+			assert_eq!(
+				bid,
+				BidOrder {
+					blockchain: old_bid.blockchain,
+					borrower_address_id: old_bid.borrower_address_id,
+					terms: BidTerms(LoanTerms {
+						amount: amount.clone(),
+						interest_rate: InterestRate {
+							rate_per_period: old_bid.terms.0.interest_rate,
+							decimals: OLD_INTEREST_RATE_DECIMALS,
+							period: Duration::from_millis(old_bid.terms.0.maturity)
+						},
+						term_length: Duration::from_millis(old_bid.terms.0.maturity)
+					}),
+					expiration_block,
+					block,
+					borrower: test_info.borrower.account_id.clone(),
+				}
+			)
+		})
+	}
+
+	#[test]
+	fn deal_order_migrates() {
+		ExtBuilder::default().build_and_execute(|| {
+			let offer_id = OfferId::with_expiration_hash::<Test>(100, [1; 32].into());
+			let deal_id = DealOrderId::with_expiration_hash::<Test>(100, [0; 32].into());
+			let test_info = TestInfo::new_defaults();
+			let old_deal = OldDealOrder {
+				blockchain: Blockchain::Ethereum,
+				lender_address_id: test_info.lender.address_id.clone(),
+				terms: OldLoanTerms { amount: 1000u64.into(), interest_rate: 1000, maturity: 2000 },
+				expiration_block: 100,
+				offer_id: offer_id.clone(),
+				borrower_address_id: test_info.borrower.address_id.clone(),
+				timestamp: 0,
+				funding_transfer_id: None,
+				repayment_transfer_id: None,
+				lock: None,
+				borrower: test_info.borrower.account_id.clone(),
+			};
+
+			OldDealOrders::insert_id(deal_id.clone(), old_deal.clone());
+
+			super::migrate::<Test>();
+
+			let deal = super::DealOrders::<Test>::try_get_id(&deal_id).unwrap();
+
+			assert_eq!(
+				deal,
+				super::DealOrder {
+					blockchain: old_deal.blockchain,
+					lender_address_id: old_deal.lender_address_id,
+					terms: LoanTerms {
+						amount: old_deal.terms.amount,
+						interest_rate: InterestRate {
+							rate_per_period: old_deal.terms.interest_rate,
+							decimals: OLD_INTEREST_RATE_DECIMALS,
+							period: Duration::from_millis(old_deal.terms.maturity)
+						},
+						term_length: Duration::from_millis(
+							old_deal.terms.maturity.saturating_sub(old_deal.timestamp)
+						)
+					},
+					offer_id: old_deal.offer_id,
+					borrower_address_id: old_deal.borrower_address_id,
+					expiration_block: old_deal.expiration_block,
+					timestamp: old_deal.timestamp,
+					funding_transfer_id: old_deal.funding_transfer_id,
+					repayment_transfer_id: old_deal.repayment_transfer_id,
+					lock: old_deal.lock,
+					borrower: old_deal.borrower
+				}
+			);
+		});
+	}
 }
