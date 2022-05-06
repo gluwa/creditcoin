2984b5e85f96c70b0bfca13b93fc2d36db392f52
Jeremy Frank <jeremy.frank@gluwa.com>
2022-05-06 16:52:38 -0600

    creditcoin-js: add CreditcoinApi type

--- creditcoin-js/src/creditcoin-api.ts
+++ creditcoin-js/src/creditcoin-api.ts
@@ -1,8 +1,9 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { extrinsics } from './extrinsics/extrinsics';
 import { utils } from './utils';
+import { CreditcoinApi } from './types';
 
-export const creditcoinApi = async (wsUrl: string) => {
+export const creditcoinApi = async (wsUrl: string): Promise<CreditcoinApi> => {
     const provider = new WsProvider(wsUrl);
     const api = await ApiPromise.create({ provider });
 
--- creditcoin-js/src/types.ts
+++ creditcoin-js/src/types.ts
@@ -1,4 +1,104 @@
-import { SubmittableResult } from '@polkadot/api';
+import { ApiPromise, SubmittableResult } from '@polkadot/api';
+import { KeyringPair } from '@polkadot/keyring/types';
+import { BN } from '@polkadot/util';
+import { Guid } from 'js-guid';
+import {
+    AccountId,
+    AddressId,
+    AskOrderId,
+    BidOrderId,
+    Blockchain,
+    DealOrderAdded,
+    DealOrderClosed,
+    DealOrderFunded,
+    DealOrderId,
+    DealOrderLocked,
+    LoanTerms,
+    OfferId,
+    TransferId,
+    TransferKind,
+    TransferProcessed,
+} from './model';
+import { AddressRegistered } from './extrinsics/register-address';
+import { AskOrderAdded } from './extrinsics/add-ask-order';
+import { BidOrderAdded } from './extrinsics/add-bid-order';
+import { OfferAdded } from './extrinsics/add-offer';
+import { DealOrderRegistered } from './extrinsics/register-deal-order';
+import { TransferEvent } from './extrinsics/register-transfers';
+import { LoanExempted } from './extrinsics/exempt';
+import { Wallet } from 'ethers';
 
 export type TxCallback = (result: SubmittableResult) => void;
 export type ExtrinsicFailed = string;
+
+export interface Extrinsics {
+    registerAddress: (
+        externalAddress: string,
+        blockchain: Blockchain,
+        ownershipProof: string,
+        signer: KeyringPair,
+    ) => Promise<AddressRegistered>;
+    addAskOrder: (
+        lenderAddressId: AddressId,
+        loanTerms: LoanTerms,
+        expirationBlock: number,
+        guid: Guid,
+        signer: KeyringPair,
+    ) => Promise<AskOrderAdded>;
+    addBidOrder: (
+        borrowerAddressId: AddressId,
+        loanTerms: LoanTerms,
+        expirationBlock: number,
+        guid: Guid,
+        signer: KeyringPair,
+    ) => Promise<BidOrderAdded>;
+    addOffer: (
+        askOrderId: AskOrderId,
+        bidOrderId: BidOrderId,
+        expirationBlock: number,
+        signer: KeyringPair,
+    ) => Promise<OfferAdded>;
+    addDealOrder: (offerId: OfferId, expirationBlock: number, signer: KeyringPair) => Promise<DealOrderAdded>;
+    registerDealOrder: (
+        lenderAddressId: AddressId,
+        borrowerAddressId: AddressId,
+        loanTerms: LoanTerms,
+        expBlock: number,
+        askGuid: Guid,
+        bidGuid: Guid,
+        borrowerKey: Uint8Array,
+        signedParams: Uint8Array,
+        lender: KeyringPair,
+    ) => Promise<DealOrderRegistered>;
+    registerFundingTransfer: (
+        transferKind: TransferKind,
+        dealOrderId: DealOrderId,
+        txHash: string,
+        lender: KeyringPair,
+    ) => Promise<TransferEvent>;
+    fundDealOrder: (
+        dealOrderId: DealOrderId,
+        transferId: TransferId,
+        lender: KeyringPair,
+    ) => Promise<[DealOrderFunded, TransferProcessed]>;
+    lockDealOrder: (dealOrderId: DealOrderId, borrower: KeyringPair) => Promise<DealOrderLocked>;
+    registerRepaymentTransfer: (
+        transferKind: TransferKind,
+        repaymentAmount: BN,
+        dealOrderId: DealOrderId,
+        txHash: string,
+        borrower: KeyringPair,
+    ) => Promise<TransferEvent>;
+    closeDealOrder: (
+        dealOrderId: DealOrderId,
+        transferId: TransferId,
+        borrower: KeyringPair,
+    ) => Promise<[DealOrderClosed, TransferProcessed]>;
+    exemptLoan: (dealOrderId: DealOrderId, lender: KeyringPair) => Promise<LoanExempted>;
+}
+
+export interface CreditcoinApi {
+    api: ApiPromise;
+    extrinsics: Extrinsics;
+    utils: { signAccountId: (signer: Wallet, accountId: AccountId) => string };
+}
