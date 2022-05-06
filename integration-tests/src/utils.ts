6214f172a7b199ccf06e38dde2fba02f06786c00
Jeremy Frank <jeremy.frank@gluwa.com>
2022-05-06 13:15:38 -0600

    initial updated integration-tests project

---
+++ integration-tests/src/utils.ts
@@ -0,0 +1,51 @@
+/* eslint-disable */
+import { ApiPromise } from '@polkadot/api';
+import type { EventRecord } from '@polkadot/types/interfaces/system';
+import type { Balance, DispatchError } from '@polkadot/types/interfaces';
+
+const expectNoDispatchError = (api: ApiPromise, dispatchError?: DispatchError): void => {
+    if (dispatchError) {
+        if (dispatchError.isModule) {
+            const decoded = api.registry.findMetaError(dispatchError.asModule);
+            const { docs, name, section } = decoded;
+
+            expect(`${section}.${name}: ${docs.join(' ')}`).toBe('');
+        } else {
+            expect(dispatchError.toString()).toBe('');
+        }
+    }
+};
+
+export const extractFee = async (
+    resolve: any,
+    reject: any,
+    unsubscribe: any,
+    api: ApiPromise,
+    dispatchError: DispatchError | undefined,
+    events: EventRecord[],
+    status: any,
+): Promise<void> => {
+    expectNoDispatchError(api, dispatchError);
+    if (status.isInBlock) {
+        const balancesWithdraw = events.find(({ event: { method, section } }) => {
+            return section === 'balances' && method === 'Withdraw';
+        });
+
+        expect(balancesWithdraw).toBeTruthy();
+
+        if (balancesWithdraw) {
+            const fee = (balancesWithdraw.event.data[1] as Balance).toBigInt();
+
+            const unsub = await unsubscribe;
+
+            if (unsub) {
+                unsub();
+                resolve(fee);
+            } else {
+                reject(new Error('Subscription failed'));
+            }
+        } else {
+            reject(new Error("Fee wasn't found"));
+        }
+    }
+};
