3719a8de468f41bd3a458e564e4c0818640e86ee
Jeremy Frank <jeremy.frank@gluwa.com>
2022-05-09 10:45:08 -0600

    integration-tests: add add-offer tests

--- integration-tests/src/test/common.ts
+++ integration-tests/src/test/common.ts
@@ -1,6 +1,9 @@
 import { Wallet } from 'ethers';
+import { Guid } from 'js-guid';
 import { BN } from '@polkadot/util';
+import { KeyringPair } from '@polkadot/keyring/types';
 import { Blockchain, LoanTerms } from 'creditcoin-js/model';
+import { CreditcoinApi } from 'creditcoin-js/types';
 import { Keyring } from '@polkadot/api';
 
 type TestData = {
@@ -31,4 +34,29 @@ export const testData: TestData = {
     expirationBlock: 10_000,
     createWallet: Wallet.createRandom,
     keyring: new Keyring({ type: 'sr25519' }),
+};
+
+export const addAskAndBidOrder = async (ccApi: CreditcoinApi, lender: KeyringPair, borrower: KeyringPair) => {
+    const {
+        extrinsics: { addAskOrder, addBidOrder, registerAddress },
+        utils: { signAccountId },
+    } = ccApi;
+
+    const { blockchain, expirationBlock, loanTerms } = testData;
+    const lenderWallet = Wallet.createRandom();
+    const borrowerWallet = Wallet.createRandom();
+
+    const [lenderRegAddr, borrowerRegAddr] = await Promise.all([
+        registerAddress(lenderWallet.address, blockchain, signAccountId(lenderWallet, lender.address), lender),
+        registerAddress(borrowerWallet.address, blockchain, signAccountId(borrowerWallet, borrower.address), borrower),
+    ]);
+    const askGuid = Guid.newGuid();
+    const bidGuid = Guid.newGuid();
+
+    const [askOrderAdded, bidOrderAdded] = await Promise.all([
+        addAskOrder(lenderRegAddr.itemId, loanTerms, expirationBlock, askGuid, lender),
+        addBidOrder(borrowerRegAddr.itemId, loanTerms, expirationBlock, bidGuid, borrower),
+    ]);
+
+    return [askOrderAdded.itemId, bidOrderAdded.itemId];
 };
