141df1b46c39d15178610f2a8a63c3bc0ae42a2b
Alex Todorov <alex.todorov@gluwa.com>
2022-05-12 15:21:48 +0000

    Leave only the new integration tests in CI and remove the old

--- javascript/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-// Copyright 2017-2022 @polkadot/api authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-const config = require('@polkadot/dev/config/jest.cjs');
-
-module.exports = {
-    ...config,
-    testTimeout: 30000,
-};
