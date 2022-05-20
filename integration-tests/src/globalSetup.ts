d61435dffd8b42bfc852fd63db8acc1eea588c32
Alex Todorov <alex.todorov@gluwa.com>
2022-05-20 13:49:49 +0000

    tests: Execute setupAuthority() conditionally
    b/c it may not be needed when testing aginst testnet

--- integration-tests/src/globalSetup.ts
+++ integration-tests/src/globalSetup.ts
@@ -4,11 +4,25 @@ import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
 const setup = async () => {
     process.env.NODE_ENV = 'test';
 
+    if (!(global as any).CREDITCOIN_API_URL) {
+        (global as any).CREDITCOIN_API_URL = 'ws://127.0.0.1:9944';
+    }
+
+    if (!(global as any).CREDITCOIN_METRICS_BASE) {
+        (global as any).CREDITCOIN_METRICS_BASE = 'http://127.0.0.1:9615';
+    }
+
+    if (!(global as any).CREDITCOIN_EXECUTE_SETUP_AUTHORITY) {
+        (global as any).CREDITCOIN_EXECUTE_SETUP_AUTHORITY = true;
+    }
+
     const api = await ApiPromise.create({
         provider: new WsProvider((global as any).CREDITCOIN_API_URL),
     });
-    const alice = new Keyring({ type: 'sr25519' }).addFromUri('//Alice');
-    await setupAuthority(api, alice);
+    if ((global as any).CREDITCOIN_EXECUTE_SETUP_AUTHORITY) {
+        const alice = new Keyring({ type: 'sr25519' }).addFromUri('//Alice');
+        await setupAuthority(api, alice);
+    }
     await api.disconnect();
 };
 
