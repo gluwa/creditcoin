49097f10c4721c85adad249e4107a4ff356c524d
Jeremy Frank <jeremy.frank@gluwa.com>
2022-06-16 17:48:14 -0600

    add yarn workspaces and config updates trying to get jest to work, it doesn't

--- integration-tests/jest.config.ts
+++ integration-tests/jest.config.ts
@@ -1,9 +1,14 @@
-import type { Config } from "@jest/types";
+import type { Config } from '@jest/types';
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testTimeout: 30000,
-  globalSetup: "./src/globalSetup.ts",
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testTimeout: 30000,
+    globalSetup: './src/globalSetup.ts',
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+    modulePaths: ['<rootDir>'],
 };
 
 export default config;
