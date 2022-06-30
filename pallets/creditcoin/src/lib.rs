fefab472a935887d22d2c7a2a640e6833be90029
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-06-30 16:34:46 -0400

    Remove OrderId from transfer (and everything else)

--- pallets/creditcoin/src/lib.rs
+++ pallets/creditcoin/src/lib.rs
@@ -985,7 +985,7 @@ pub mod pallet {
 				},
 				|transfer, deal_order| {
 					ensure!(
-						transfer.order_id == OrderId::Deal(deal_order_id.clone()),
+						transfer.deal_order_id == deal_order_id.clone(),
 						Error::<T>::TransferDealOrderMismatch
 					);
 					ensure!(
@@ -1145,7 +1145,7 @@ pub mod pallet {
 				},
 				|transfer, _deal_order| {
 					ensure!(
-						transfer.order_id == OrderId::Deal(deal_order_id.clone()),
+						transfer.deal_order_id == deal_order_id.clone(),
 						Error::<T>::TransferDealOrderMismatch
 					);
 
@@ -1219,7 +1219,7 @@ pub mod pallet {
 				order.borrower_address_id,
 				transfer_kind,
 				order.terms.amount,
-				OrderId::Deal(deal_order_id),
+				deal_order_id,
 				blockchain_tx_id,
 			)?;
 			Self::deposit_event(Event::<T>::TransferRegistered(transfer_id, transfer));
@@ -1246,7 +1246,7 @@ pub mod pallet {
 				order.lender_address_id,
 				transfer_kind,
 				repayment_amount,
-				OrderId::Deal(deal_order_id),
+				deal_order_id,
 				blockchain_tx_id,
 			)?;
 			Self::deposit_event(Event::<T>::TransferRegistered(transfer_id, transfer));
@@ -1276,7 +1276,7 @@ pub mod pallet {
 					ensure!(who == lender.owner, Error::<T>::NotLender);
 
 					let fake_transfer = Transfer {
-						order_id: OrderId::Deal(deal_order_id.clone()),
+						deal_order_id: deal_order_id.clone(),
 						block: Self::block_number(),
 						account_id: who,
 						amount: ExternalAmount::zero(),
--- pallets/creditcoin/src/migrations/v6.rs
+++ pallets/creditcoin/src/migrations/v6.rs
@@ -1,6 +1,8 @@
 use super::v5;
 use crate::Config;
+use frame_support::generate_storage_alias;
 use frame_support::pallet_prelude::*;
+
 pub use v5::AskOrder as OldAskOrder;
 pub use v5::AskTerms as OldAskTerms;
 pub use v5::BidOrder as OldBidOrder;
@@ -12,6 +14,46 @@ pub use v5::OrderId as OldOrderId;
 pub use v5::Transfer as OldTransfer;
 pub use v5::TransferKind as OldTransferKind;
 
+use crate::Transfer;
+use crate::TransferId;
+
+generate_storage_alias!(
+	Creditcoin,
+	Transfers<T: Config> => Map<(Identity, TransferId<T::Hash>), Transfer<T::AccountId, T::BlockNumber, T::Hash, T::Moment>>
+);
+
+#[allow(unreachable_code)]
 pub(crate) fn migrate<T: Config>() -> Weight {
-	todo!()
+	let mut weight: Weight = 0;
+	let weight_each = T::DbWeight::get().reads_writes(1, 1);
+
+	Transfers::<T>::translate::<OldTransfer<T::AccountId, T::BlockNumber, T::Hash, T::Moment>, _>(
+		|_id, transfer| {
+			weight = weight.saturating_add(weight_each);
+			Some(Transfer {
+				amount: transfer.amount,
+				from: transfer.from,
+				to: transfer.to,
+				tx_id: transfer.tx_id,
+				block: transfer.block,
+				is_processed: transfer.is_processed,
+				account_id: transfer.account_id,
+				timestamp: transfer.timestamp,
+				deal_order_id: match transfer.order_id {
+					OldOrderId::Deal(id) => id,
+					OldOrderId::Repayment(id) => {
+						log::warn!(
+							"Found unexpected repayment ID attached to a transfer: {:?}",
+							id
+						);
+						return None;
+					},
+				},
+				blockchain: todo!(),
+				kind: todo!(),
+			})
+		},
+	);
+
+	weight
 }
