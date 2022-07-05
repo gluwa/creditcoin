65c17dcfdfaf33a93d6a58ea78814ae488966655
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-07-05 16:03:48 -0400

    mark legacy transfer functions

--- pallets/creditcoin/src/lib.rs
+++ pallets/creditcoin/src/lib.rs
@@ -1195,7 +1195,7 @@ pub mod pallet {
 
 			let order = try_get_id!(DealOrders<T>, &deal_order_id, NonExistentDealOrder)?;
 
-			let (transfer_id, transfer) = Self::register_transfer_internal(
+			let (transfer_id, transfer) = Self::register_transfer_internal_legacy(
 				who,
 				order.lender_address_id,
 				order.borrower_address_id,
@@ -1222,7 +1222,7 @@ pub mod pallet {
 
 			let order = try_get_id!(DealOrders<T>, &deal_order_id, NonExistentDealOrder)?;
 
-			let (transfer_id, transfer) = Self::register_transfer_internal(
+			let (transfer_id, transfer) = Self::register_transfer_internal_legacy(
 				who,
 				order.borrower_address_id,
 				order.lender_address_id,
