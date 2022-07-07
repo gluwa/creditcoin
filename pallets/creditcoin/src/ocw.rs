41b582a2a394d52ad8500dd2cdd39ac0ecec5989
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-07-07 18:15:30 -0400

    Migrate to new blockchain

--- pallets/creditcoin/src/ocw.rs
+++ pallets/creditcoin/src/ocw.rs
@@ -2,7 +2,7 @@ pub mod errors;
 pub mod rpc;
 pub mod tasks;
 
-use crate::{Call, LegacyTransferKind, OldBlockchain};
+use crate::{Blockchain, Call};
 pub use errors::{OffchainError, VerificationFailureCause, VerificationResult};
 
 use self::errors::RpcUrlError;
@@ -18,29 +18,22 @@ use sp_std::prelude::*;
 
 pub type OffchainResult<T, E = errors::OffchainError> = Result<T, E>;
 
-impl OldBlockchain {
-	pub fn rpc_url(&self) -> OffchainResult<String, errors::RpcUrlError> {
-		let chain_prefix = self.as_bytes();
-		let mut buf = Vec::from(chain_prefix);
-		buf.extend("-rpc-uri".bytes());
-		let rpc_url_storage = StorageValueRef::persistent(&buf);
+impl Blockchain {
+	pub fn rpc_url(&self) -> OffchainResult<String, RpcUrlError> {
+		let key = self.rpc_key();
+		let rpc_url_storage = StorageValueRef::persistent(&key);
 		if let Some(url_bytes) = rpc_url_storage.get::<Vec<u8>>()? {
 			Ok(String::from_utf8(url_bytes)?)
 		} else {
 			Err(RpcUrlError::NoValue)
 		}
 	}
-	pub fn supports(&self, kind: &LegacyTransferKind) -> bool {
-		match (self, kind) {
-			(
-				OldBlockchain::Ethereum | OldBlockchain::Luniverse | OldBlockchain::Rinkeby,
-				LegacyTransferKind::Erc20(_)
-				| LegacyTransferKind::Ethless(_)
-				| LegacyTransferKind::Native,
-			) => true,
-			(OldBlockchain::Bitcoin, LegacyTransferKind::Native) => true,
-			(_, _) => false, // TODO: refine this later
-		}
+
+	pub fn rpc_key(&self) -> Vec<u8> {
+		let chain_prefix = self.as_bytes();
+		let mut buf = Vec::from(chain_prefix);
+		buf.extend("-rpc-uri".bytes());
+		buf
 	}
 }
 
