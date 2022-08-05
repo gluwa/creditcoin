fe39cdbb4c3e136a3344f8e07c1db410042b7dea
Carlos Alejandro Gutiérrez Sandoval <calgusa@gmail.com>
2022-08-04 19:59:49 -0500

    Revert "Add tests creditcoin_eventsSubscribe() & creditcoin_eventsUnsubscribe()"
    This reverts commit cc02df84f244bf43966d9812ab6edcef1bbffcc9.

--- integration-tests/src/test/rpc/creditcoin.test.ts
+++ integration-tests/src/test/rpc/creditcoin.test.ts
@@ -14,23 +14,6 @@ describe('Creditcoin RPC', (): void => {
             provider,
             rpc: {
                 creditcoin: {
-                    getEvents: {
-                        params: [
-                            {
-                                isOptional: true,
-                                name: 'at',
-                                type: 'Hash',
-                            },
-                        ],
-                        description: 'Get events in a json format',
-                        type: 'Vec<Json>',
-                    },
-                    eventsSubscribe: {
-                        params: [],
-                        description: 'Subscribe to events',
-                        type: 'Subscription<Json>',
-                        pubsub: ['events', 'eventsSubscribe', 'eventsUnsubscribe'],
-                    },
                     hashrate: {
                         params: [],
                         description: 'Get hashrate',
@@ -43,111 +26,6 @@ describe('Creditcoin RPC', (): void => {
 
     afterEach(async () => {
         await api.disconnect();
-    });
-
-    it('getEvents() should return some events', (): void => {
-        return (api.rpc as any).creditcoin.getEvents().then((result: any) => {
-            // in case of failures should be easy to spot what went wrong
-            console.log(`**** RESULT=${result.toString() as string}`);
-
-            expect(result.isEmpty).toBeFalsy();
-            expect(result.toJSON()).toEqual(expect.anything());
-        });
-    });
-
-    it('eventsSubscribe() should receive events', (done): void => {
-        expect.assertions(1);
-
-        let subscriptionId: string;
-        const ws = new WebSocket((global as any).CREDITCOIN_API_URL);
-
-        ws.on('open', () => {
-            const rpc = { id: 1, jsonrpc: '2.0', method: 'creditcoin_eventsSubscribe' };
-            ws.send(JSON.stringify(rpc));
-        })
-            .on('message', (data) => {
-                const utf8Str = data.toString('utf-8');
-                // console.log('decoded-message', utf8Str);
-
-                if (!subscriptionId) {
-                    subscriptionId = JSON.parse(utf8Str).result;
-                } else {
-                    // assert at least one message is received
-                    const parsedData = JSON.parse(utf8Str);
-                    expect(parsedData).toBeTruthy();
-                    ws.close();
-                }
-            })
-            .on('close', () => done());
-    });
-
-    it('eventsUnsubscribe() should return true', (done): void => {
-        expect.assertions(1);
-
-        let subscriptionId: string;
-        const ws = new WebSocket((global as any).CREDITCOIN_API_URL);
-
-        ws.on('open', () => {
-            const rpc = { id: 1, jsonrpc: '2.0', method: 'creditcoin_eventsSubscribe' };
-            ws.send(JSON.stringify(rpc));
-        })
-            .on('message', (data) => {
-                const utf8Str = data.toString('utf-8');
-                // console.log('decoded-message', utf8Str);
-
-                if (!subscriptionId) {
-                    subscriptionId = JSON.parse(utf8Str).result;
-                    // unsubscribe
-                    const rpc = {
-                        id: 1,
-                        jsonrpc: '2.0',
-                        method: 'creditcoin_eventsUnsubscribe',
-                        params: [subscriptionId],
-                    };
-
-                    ws.send(JSON.stringify(rpc));
-                } else {
-                    const parsedData = JSON.parse(utf8Str);
-                    expect(parsedData.result).toBe(true);
-                    ws.close();
-                }
-            })
-            .on('close', () => done());
-    });
-
-    it('eventsUnsubscribe() handles invalid subscription id', (done): void => {
-        expect.assertions(1);
-
-        let firstTime = true;
-        const ws = new WebSocket((global as any).CREDITCOIN_API_URL);
-
-        ws.on('open', () => {
-            const rpc = { id: 1, jsonrpc: '2.0', method: 'creditcoin_eventsSubscribe' };
-            ws.send(JSON.stringify(rpc));
-        })
-            .on('message', (data) => {
-                const utf8Str = data.toString('utf-8');
-                // console.log('decoded-message', utf8Str);
-
-                if (firstTime) {
-                    firstTime = false;
-
-                    // unsubscribe
-                    const rpc = {
-                        id: 1,
-                        jsonrpc: '2.0',
-                        method: 'creditcoin_eventsUnsubscribe',
-                        params: ['invalid-id'],
-                    };
-
-                    ws.send(JSON.stringify(rpc));
-                } else {
-                    const parsedData = JSON.parse(utf8Str);
-                    expect(parsedData.error.message).toBe('Invalid subscription id.');
-                    ws.close();
-                }
-            })
-            .on('close', () => done());
     });
 
     it('hashrate() should return a valid hashrate', async () => {
