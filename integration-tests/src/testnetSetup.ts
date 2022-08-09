9f38c7f2c92eeccbf29dbfb8e9a87baf387f937f
Alex Todorov <alex.todorov@gluwa.com>
2022-08-09 11:51:57 +0000

    tests: Update URLs for testnet

--- integration-tests/src/testnetSetup.ts
+++ integration-tests/src/testnetSetup.ts
@@ -19,7 +19,7 @@ const createWallet = (who: 'lender' | 'borrower') => {
 };
 
 const setup = async () => {
-    (global as any).CREDITCOIN_API_URL = 'wss://testnet.creditcoin.network';
+    (global as any).CREDITCOIN_API_URL = 'wss://rpc.testnet.creditcoin.network/ws';
     (global as any).CREDITCOIN_CREATE_WALLET = createWallet;
 
     (global as any).CREDITCOIN_ETHEREUM_DECREASE_MINING_INTERVAL = false;
@@ -34,7 +34,7 @@ const setup = async () => {
     (global as any).CREDITCOIN_EXECUTE_SETUP_AUTHORITY = false;
     (global as any).CREDITCOIN_NETWORK_LONG_NAME = 'Creditcoin Testnet';
     (global as any).CREDITCOIN_NETWORK_SHORT_NAME = 'creditcoin_testnet';
-    (global as any).CREDITCOIN_METRICS_BASE = 'http://cctn-rpc.francecentral.azurecontainer.io:9615';
+    (global as any).CREDITCOIN_METRICS_BASE = 'http://test-rpc-creditcoin-rpc-2.eastus.cloudapp.azure.com:9615';
     (global as any).CREDITCOIN_REUSE_EXISTING_ADDRESSES = true;
 
     await globalSetup();
