a3776869467aee82b7c9ca1e25388ae7155b2114
Alex Todorov <alex.todorov@gluwa.com>
2022-08-25 12:17:41 +0000

    Add test to check for parity b/w extrinsics, benchmarks and weights

---
+++ scripts/check-parity-bw-extrinsics-benchmarks-weights.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+WHITELIST="fail_task persist_task_output"
+PALLETS=$(grep "pallet::call" pallets/*/src/lib.rs -R | cut -f2 -d/ | xargs | tr ' ' '|')
+EXTRINSICS=$(grep "pub fn" pallets/*/src/lib.rs | egrep "$PALLETS" | cut -f2 -d":" | cut -f1 -d"(" | sed 's/pub fn //' | tr -d [' \t'] | sort)
+
+echo "----- Detected extrinsics are -----"
+echo "$EXTRINSICS"
+
+for EXTRINSIC in $EXTRINSICS; do
+    if [[ $WHITELIST =~ $EXTRINSIC ]]; then
+        echo "***** Skipping $EXTRINSIC - white listed"
+        continue
+    fi
+
+    echo "----- Searching weights for $EXTRINSIC -----"
+    grep "fn $EXTRINSIC" pallets/*/src/weights.rs
+
+    # makes sure the weight for an extrinsic function has the
+    # same name as the extrinsic - in case copy&paste errors, etc
+    echo "----- Double check weights for $EXTRINSIC -----"
+    grep -B1 "pub fn $EXTRINSIC" pallets/*/src/lib.rs | grep "WeightInfo::$EXTRINSIC"
+
+    echo "----- Searching benchmarks for $EXTRINSIC -----"
+    grep "$EXTRINSIC {" pallets/*/src/benchmarking.rs
+done
+
+echo "----- DONE - ALL PASS -----"
