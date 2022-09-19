e58979f78bf919220e61eefd51dc6d9c9e48f48d
Alex Todorov <alex.todorov@gluwa.com>
2022-09-19 08:23:27 +0000

    Make sure to always call .burn() for testing against devnet/testnet
    - allows testing to be performed repeatedly
    - even if contract has been previously deployed
    - doesn't always deploy the contract but simulates actions on mainnet

--- integration-tests/src/ctc-deploy.ts
+++ integration-tests/src/ctc-deploy.ts
@@ -3,30 +3,43 @@ import { GluwaCreditVestingToken } from './ethereum/ctc/typechain';
 import CtcArtifact from './ethereum/ctc/contracts/GluwaCreditVestingToken.sol/GluwaCreditVestingToken.json';
 import { JsonRpcProvider } from '@ethersproject/providers';
 
-const deployCtcToken = async (deployer: Signer) => {
+const deployCtcToken = async (deployer: Signer, existingAddress: string | undefined) => {
     const factory = new ContractFactory(CtcArtifact.abi, CtcArtifact.bytecode, deployer);
-    const deployerAddress = await deployer.getAddress();
-    const ctcToken = (await factory.deploy(deployerAddress, deployerAddress)) as GluwaCreditVestingToken;
+    let ctcToken: GluwaCreditVestingToken;
+
+    if (existingAddress !== undefined) {
+        ctcToken = factory.attach(existingAddress) as GluwaCreditVestingToken;
+        console.log('Using existing contract', ctcToken.address);
+    } else {
+        const deployerAddress = await deployer.getAddress();
+        ctcToken = (await factory.deploy(deployerAddress, deployerAddress)) as GluwaCreditVestingToken;
+        console.log('Deployed to', ctcToken.address);
+    }
+
+    process.env.CREDITCOIN_CTC_CONTRACT_ADDRESS = ctcToken.address;
+
     return ctcToken;
 };
 
-export const main = async () => {
-    const provider = new JsonRpcProvider((global as any).CREDITCOIN_ETHEREUM_NODE_URL);
-    const deployer = new Wallet((global as any).CREDITCOIN_CTC_DEPLOYER_PRIVATE_KEY, provider);
-    const ctcToken = await deployCtcToken(deployer);
-
+const burnCtc = async (ctcToken: GluwaCreditVestingToken) => {
     const tx = await ctcToken.burn(500);
     const txHash = tx.hash;
+
     // wait for tx to be mined and get receipt
     await tx.wait();
 
-    console.log('Deployed to', ctcToken.address);
     console.log('Burn Tx hash', txHash);
-
-    process.env.CREDITCOIN_CTC_CONTRACT_ADDRESS = ctcToken.address;
     process.env.CREDITCOIN_CTC_BURN_TX_HASH = txHash;
 };
 
+export const main = async (existingAddress: string | undefined) => {
+    const provider = new JsonRpcProvider((global as any).CREDITCOIN_ETHEREUM_NODE_URL);
+    const deployer = new Wallet((global as any).CREDITCOIN_CTC_DEPLOYER_PRIVATE_KEY, provider);
+    const ctcToken = await deployCtcToken(deployer, existingAddress);
+
+    await burnCtc(ctcToken);
+};
+
 if (require.main === module) {
-    main().catch(console.error);
+    main(undefined).catch(console.error);
 }
