53fec14537381e04dbd161b84deef4f0d2aee05c
Carlos Alejandro Gutiérrez Sandoval <calgusa@gmail.com>
2022-10-05 17:10:23 -0500

    task interface
    task pallet

---
+++ pallets/offchain-task-scheduler/src/lib.rs
@@ -0,0 +1,198 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+extern crate alloc;
+use frame_support::traits::Get;
+use frame_system::offchain::AppCrypto;
+use frame_system::pallet_prelude::BlockNumberFor;
+use frame_system::Config as SystemConfig;
+pub use pallet::{Authorities, Config, Error, Event, Pallet, WeightInfo};
+pub use pallet::{__substrate_call_check, __substrate_event_check};
+use sp_core::offchain::KeyTypeId;
+use sp_runtime::traits::BlockNumberProvider;
+use sp_runtime::traits::Saturating;
+use sp_tracing as log;
+
+mod ocw;
+pub mod tasks;
+#[allow(clippy::unnecessary_cast)]
+pub mod weights;
+
+//gluwa's offchain task scheduler
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"gots");
+
+pub mod crypto {
+	use super::AppCrypto;
+	use crate::KEY_TYPE;
+	use sp_runtime::{
+		app_crypto::{app_crypto, sr25519},
+		MultiSignature, MultiSigner,
+	};
+
+	app_crypto!(sr25519, KEY_TYPE);
+
+	pub struct AuthorityId;
+
+	impl AppCrypto<MultiSigner, MultiSignature> for AuthorityId {
+		type RuntimeAppPublic = Public;
+		type GenericPublic = sp_core::sr25519::Public;
+		type GenericSignature = sp_core::sr25519::Signature;
+	}
+}
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::{
+		log,
+		tasks::{self, VerifiableTask},
+		AppCrypto, Saturating, SystemConfig,
+	};
+	use codec::FullCodec;
+	use core::fmt::Debug;
+	use frame_support::dispatch::Dispatchable;
+	use frame_support::pallet_prelude::*;
+	use frame_system::offchain::CreateSignedTransaction;
+	use frame_system::pallet_prelude::*;
+	use scale_info::TypeInfo;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config + CreateSignedTransaction<Self::TaskCall> {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type Task: TypeInfo + FullCodec + MaxEncodedLen + VerifiableTask<Self> + Debug;
+		type UnverifiedTaskTimeout: Get<<Self as SystemConfig>::BlockNumber>;
+		type WeightInfo: WeightInfo;
+		type AuthorityId: AppCrypto<
+			Self::Public,
+			<Self as frame_system::offchain::SigningTypes>::Signature,
+		>;
+		type AccountIdFrom: From<sp_core::sr25519::Public>
+			+ IsType<Self::AccountId>
+			+ Clone
+			+ core::fmt::Debug
+			+ PartialEq
+			+ AsRef<[u8; 32]>;
+
+		type InternalPublic: sp_core::crypto::UncheckedFrom<[u8; 32]>;
+		type PublicSigning: From<Self::InternalPublic> + Into<Self::Public>;
+		type TaskCall: Dispatchable<Origin = Self::Origin> + Clone;
+	}
+
+	pub trait WeightInfo {
+		fn on_initialize(p: u32) -> Weight;
+	}
+
+	#[pallet::event]
+	pub enum Event<T: Config> {}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::storage]
+	#[pallet::getter(fn pending_tasks)]
+	pub type PendingTasks<T: Config> =
+		StorageDoubleMap<_, Identity, T::BlockNumber, Identity, T::Hash, T::Task>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn authorities)]
+	pub type Authorities<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, ()>;
+
+	#[derive(PartialEq, Eq)]
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Failed to send an offchain callback transaction. This is likely
+		/// an internal error.
+		OffchainSignedTxFailed,
+		/// The node is an authority but there is no account to create a
+		/// callback transaction. This is likely an internal error.
+		NoLocalAcctForSignedTx,
+	}
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_initialize(block_number: T::BlockNumber) -> Weight {
+			use sp_io::KillStorageResult;
+
+			log::debug!("Cleaning up expired entries");
+
+			let mut unverified_task_count = 0u32;
+			loop {
+				match PendingTasks::<T>::remove_prefix(block_number, None) {
+					KillStorageResult::SomeRemaining(u) => {
+						unverified_task_count.saturating_accrue(u)
+					},
+					KillStorageResult::AllRemoved(u) => {
+						unverified_task_count.saturating_accrue(u);
+						break;
+					},
+				};
+			}
+
+			<T as Config>::WeightInfo::on_initialize(unverified_task_count)
+		}
+
+		fn offchain_worker(block_number: T::BlockNumber) {
+			let auth_id = match Self::authority_id() {
+				Some(id) => id,
+				None => {
+					log::debug!(target: "task", "Not an authority, skipping offchain work");
+					return;
+				},
+			};
+
+			for (deadline, id, task) in PendingTasks::<T>::iter() {
+				let storage_key = tasks::storage_key(&id);
+				let mut lock = tasks::task_lock::<T>(&storage_key);
+
+				let guard = match lock.try_lock() {
+					Ok(g) => g,
+					Err(_) => continue,
+				};
+
+				log::trace!(target: "task", "@{block_number:?} Task {:8?}", id);
+
+				use tasks::error::TaskError::*;
+				match task.forward_task(deadline) {
+					Ok(call) => {
+						match Self::submit_txn_with_synced_nonce(auth_id.clone(), |_| call.clone())
+						{
+							Ok(_) => guard.forget(),
+							Err(e) => {
+								log::error!("Failed to send a dispatchable transaction: {:?}", e)
+							},
+						}
+					},
+					Err(FinishedTask) => {
+						log::debug!("Already handled Task ({:?}, {:?}) {task:?}", deadline, id);
+						guard.forget();
+						continue;
+					},
+					Err(Evaluation(cause)) => {
+						log::warn!("Failed to verify pending task {:?} : {:?}", task, cause);
+					},
+					Err(Scheduler(error)) => {
+						log::error!("Task verification encountered a processing error {:?}", error)
+					},
+				}
+			}
+		}
+	}
+}
+
+type TaskFor<T> = <T as Config>::Task;
+type HashFor<T> = <T as SystemConfig>::Hash;
+
+impl<Runtime: Config>
+	tasks::TaskScheduler<BlockNumberFor<Runtime>, HashFor<Runtime>, TaskFor<Runtime>> for Runtime
+{
+	fn deadline() -> BlockNumberFor<Runtime> {
+		let offset = Runtime::UnverifiedTaskTimeout::get();
+		let block = frame_system::Pallet::<Runtime>::current_block_number();
+		offset.saturating_add(block).into()
+	}
+	fn is_scheduled(deadline: &BlockNumberFor<Runtime>, id: &HashFor<Runtime>) -> bool {
+		crate::pallet::PendingTasks::<Runtime>::contains_key(deadline, id)
+	}
+	fn insert(deadline: &BlockNumberFor<Runtime>, id: &HashFor<Runtime>, task: TaskFor<Runtime>) {
+		crate::pallet::PendingTasks::<Runtime>::insert(deadline, id, task);
+	}
+}
---
+++ pallets/offchain-task-scheduler/src/ocw.rs
@@ -0,0 +1,84 @@
+pub(crate) mod nonce;
+
+use super::Error;
+use super::{crypto, log, Authorities, Config, Pallet};
+use alloc::vec;
+use frame_support::dispatch::Vec;
+use frame_support::traits::IsType;
+use frame_system::offchain::{Account, SendSignedTransaction, Signer};
+use frame_system::{Config as SystemConfig, Pallet as System};
+use nonce::{lock_key, nonce_key};
+use sp_runtime::offchain::storage::StorageValueRef;
+use sp_runtime::traits::One;
+use sp_runtime::traits::Saturating;
+use sp_runtime::RuntimeAppPublic;
+
+impl<T: Config> Pallet<T> {
+	pub fn authority_id() -> Option<T::AccountIdFrom> {
+		let local_keys = crypto::Public::all()
+			.into_iter()
+			.map(|p| sp_core::sr25519::Public::from(p).into())
+			.collect::<Vec<T::AccountIdFrom>>();
+
+		log::trace!(target: "task", "local keys {local_keys:?}");
+
+		Authorities::<T>::iter_keys().find_map(|auth| {
+			let acct = auth.clone().into();
+			local_keys.contains(&acct).then(|| T::AccountIdFrom::from(auth))
+		})
+	}
+}
+
+impl<T: Config> Pallet<T> {
+	fn offchain_signed_tx(
+		auth_id: T::AccountIdFrom,
+		call: impl Fn(&Account<T>) -> T::TaskCall,
+	) -> Result<(), Error<T>> {
+		use sp_core::crypto::UncheckedFrom;
+		let auth_bytes: &[u8; 32] = auth_id.as_ref();
+		let public: T::PublicSigning = T::InternalPublic::unchecked_from(*auth_bytes).into();
+		let signer = Signer::<T, T::AuthorityId>::any_account().with_filter(vec![public.into()]);
+		let result = signer.send_signed_transaction(call);
+
+		if let Some((acc, res)) = result {
+			if res.is_err() {
+				log::error!("failure: offchain_signed_tx: tx sent: {:?}", acc.id);
+				return Err(Error::OffchainSignedTxFailed);
+			} else {
+				return Ok(());
+			}
+		}
+
+		log::error!("No local account available");
+		Err(Error::NoLocalAcctForSignedTx)
+	}
+
+	pub fn submit_txn_with_synced_nonce(
+		auth_id: T::AccountIdFrom,
+		call: impl Fn(&Account<T>) -> T::TaskCall,
+	) -> Result<(), Error<T>> {
+		let acc_id: &<T as SystemConfig>::AccountId = auth_id.into_ref();
+		let mut account_data = System::<T>::account(acc_id);
+
+		let key = &lock_key(auth_id.into_ref());
+		let mut lock = Pallet::<T>::nonce_lock_new(key);
+		let _guard = lock.lock();
+
+		let key = &nonce_key(auth_id.into_ref());
+		let synced_nonce_storage = StorageValueRef::persistent(key);
+		let synced_nonce = synced_nonce_storage.get::<T::Index>().ok().flatten();
+
+		let n = System::<T>::block_number();
+		log::trace!(target: "task", "@{n:?} Offnonce {synced_nonce:?} Onnonce {:?}", account_data.nonce);
+
+		if let Some(nonce) = synced_nonce {
+			if nonce > account_data.nonce {
+				account_data.nonce = nonce;
+				frame_system::Account::<T>::insert(acc_id, account_data.clone());
+			}
+		}
+
+		Pallet::<T>::offchain_signed_tx(auth_id, call)
+			.map(|_| synced_nonce_storage.set(&account_data.nonce.saturating_add(One::one())))
+	}
+}
