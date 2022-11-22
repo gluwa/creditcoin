57a16f0f7e4606b73b147953c477d65b6758bee5
Alex Todorov <alex.todorov@gluwa.com>
2022-11-22 15:26:47 +0000

    Add test for createAddressId()

--- integration-tests/src/test/register-address.test.ts
+++ integration-tests/src/test/register-address.test.ts
@@ -1,22 +1,25 @@
-import { ApiPromise, Keyring, WsProvider, KeyringPair, Wallet, POINT_01_CTC } from 'creditcoin-js';
+import { Blockchain, Keyring, KeyringPair, Wallet, POINT_01_CTC, creditcoinApi } from 'creditcoin-js';
+import { createAddressId } from 'creditcoin-js/lib/extrinsics/register-address';
 import { createCreditcoinBlockchain } from 'creditcoin-js/lib/transforms';
+import { checkAddress, testData } from 'creditcoin-js/lib/testUtils';
+import { CreditcoinApi } from 'creditcoin-js/lib/types';
 import { signAccountId } from 'creditcoin-js/lib/utils';
 import { extractFee } from '../utils';
 
 describe('RegisterAddress', () => {
-    let api: ApiPromise;
+    let ccApi: CreditcoinApi;
     let alice: KeyringPair;
 
     beforeAll(async () => {
-        api = await ApiPromise.create({
-            provider: new WsProvider((global as any).CREDITCOIN_API_URL),
-        });
+        ccApi = await creditcoinApi((global as any).CREDITCOIN_API_URL);
         alice = new Keyring({ type: 'sr25519' }).addFromUri('//Alice');
     });
 
-    afterAll(async () => await api.disconnect());
+    afterAll(async () => await ccApi.api.disconnect());
 
     it('fee is min 0.01 CTC', async (): Promise<void> => {
+        const { api } = ccApi;
+
         return new Promise((resolve, reject) => {
             const wallet = Wallet.createRandom();
             const unsubscribe = api.tx.creditcoin
@@ -32,5 +35,34 @@ describe('RegisterAddress', () => {
         }).then((fee) => {
             expect(fee).toBeGreaterThanOrEqual(POINT_01_CTC);
         });
+    });
+
+    it('createAddressId works as expected', async (): Promise<void> => {
+        const { blockchain } = testData(
+            (global as any).CREDITCOIN_ETHEREUM_CHAIN as Blockchain,
+            (global as any).CREDITCOIN_CREATE_WALLET,
+        );
+
+        const {
+            api,
+            extrinsics: { registerAddress },
+        } = ccApi;
+
+        const lender = alice;
+        const lenderWallet = Wallet.createRandom();
+        const lenderRegAddr = await registerAddress(
+            lenderWallet.address,
+            blockchain,
+            signAccountId(api, lenderWallet, lender.address),
+            lender,
+        );
+
+        // manually constructed address is the same as returned by Creditcoin
+        const addressId = createAddressId(blockchain, lenderWallet.address);
+        expect(addressId).toBe(lenderRegAddr.itemId);
+
+        // manually constructed address should be reported as registered
+        const result = await checkAddress(ccApi, addressId);
+        expect(result).toBeDefined();
     });
 });
