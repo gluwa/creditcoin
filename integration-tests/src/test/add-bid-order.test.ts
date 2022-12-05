5f53770228aa77067acf691b6dc0711c543fad58
Alex Todorov <alex.todorov@gluwa.com>
2022-12-05 14:47:41 +0000

    Replace hard-coded accounts Alice & Bob with a config function

--- integration-tests/src/test/add-bid-order.test.ts
+++ integration-tests/src/test/add-bid-order.test.ts
@@ -23,7 +23,7 @@ describe('AddBidOrder', () => {
 
     beforeAll(async () => {
         ccApi = await creditcoinApi((global as any).CREDITCOIN_API_URL);
-        borrower = keyring.addFromUri('//Bob');
+        borrower = (global as any).CREDITCOIN_CREATE_SIGNER(keyring, 'borrower');
 
         const eth = await ethConnection(
             (global as any).CREDITCOIN_ETHEREUM_NODE_URL,
