5addfb5d948352c18d6a2202ff3a4ae91ff27249
Carlos Alejandro Gutiérrez Sandoval <calgusa@gmail.com>
2022-12-13 15:51:18 -0600

    Sampling Threshold

--- runtime/src/version.rs
+++ runtime/src/version.rs
@@ -15,7 +15,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
 	spec_version: 213,
-	impl_version: 2,
+	impl_version: 3,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 10,
 	state_version: 1,
