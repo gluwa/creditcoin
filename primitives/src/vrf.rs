65b5303c9086a60055f77c1ce6573741648f31a8
Carlos Alejandro Gutiérrez Sandoval <calgusa@gmail.com>
2022-12-14 20:39:14 -0600

    vrf prove tests

--- primitives/src/vrf.rs
+++ primitives/src/vrf.rs
@@ -2,6 +2,7 @@ use num::rational::BigRational;
 use num::ToPrimitive;
 use parity_scale_codec::{Decode, Encode};
 use sp_arithmetic::per_things::{PerThing, Perbill, Perquintill};
+use sp_consensus_vrf::schnorrkel::SignatureError;
 use sp_runtime_interface::pass_by::PassByCodec;
 use sp_runtime_interface::runtime_interface;
 
@@ -166,6 +167,10 @@ pub mod sortition {
 		threshold.to_u128().expect("n * X, X ~ [0,1), n: u128; qed.")
 	}
 
+	pub fn is_selected(inout: &VRFInOut, threshold: u128) -> bool {
+		u128::from_le_bytes(inout.make_bytes::<[u8; 16]>(b"creditcoin-vrf")) < threshold
+	}
+
 	#[cfg(test)]
 	mod tests {
 		use super::*;
@@ -231,4 +236,149 @@ pub mod sortition {
 			}
 		}
 	}
+}
+
+use sp_core::H256;
+use sp_keystore::vrf::VRFTranscriptValue;
+pub use sp_keystore::vrf::{make_transcript, VRFTranscriptData};
+
+const ENGINE_ID: &[u8; 4] = b"COTS";
+
+pub(super) fn transcript_data(pre_hash: H256, epoch: u64, task_id: H256) -> VRFTranscriptData {
+	VRFTranscriptData {
+		label: ENGINE_ID,
+		items: vec![
+			("epoch", VRFTranscriptValue::U64(epoch)),
+			("task id", VRFTranscriptValue::Bytes(task_id.encode())),
+			("pre hash", VRFTranscriptValue::Bytes(pre_hash.encode())),
+		],
+	}
+}
+
+use schnorrkel::vrf::VRFInOut;
+use sp_consensus_vrf::schnorrkel::PublicKey;
+use sp_consensus_vrf::schnorrkel::{VRFOutput, VRFProof};
+use sp_core::crypto::KeyTypeId;
+use sp_core::sr25519::Public;
+#[cfg(feature = "std")]
+use sp_externalities::ExternalitiesExt;
+use sp_keystore::vrf::VRFSignature;
+#[cfg(feature = "std")]
+use sp_keystore::{KeystoreExt, SyncCryptoStore};
+use tracing as log;
+
+pub fn prove_vrf(
+	pubkey: PublicKey,
+	pre_hash: H256,
+	epoch: u64,
+	task_id: H256,
+	output: VRFOutput,
+	proof: VRFProof,
+) -> Result<VRFInOut, SignatureError> {
+	let transcript = make_transcript(transcript_data(pre_hash, epoch, task_id));
+	pubkey
+		.vrf_verify(transcript, &output, &proof)
+		.map(|(inout, _proofbatchable)| inout)
+}
+
+#[runtime_interface]
+pub trait Vrf {
+	fn generate_vrf(
+		&mut self,
+		key_type_id: KeyTypeId,
+		pubkey: &Public,
+		pre_hash: H256,
+		epoch: u64,
+		task_id: H256,
+	) -> Option<(VRFOutput, VRFProof)> {
+		let keystore = &***self
+			.extension::<KeystoreExt>()
+			.expect("No `keystore` associated for the current context!");
+		let public_data = transcript_data(pre_hash, epoch, task_id);
+		match SyncCryptoStore::sr25519_vrf_sign(keystore, key_type_id, pubkey, public_data) {
+			Ok(Some(signature)) => {
+				let VRFSignature { output, proof } = signature;
+				Some((VRFOutput(output), VRFProof(proof)))
+			},
+			Ok(None) => {
+				log::warn!(target = "VRF", "missing Public {pubkey} from {key_type_id:?}");
+				None
+			},
+			Err(e) => {
+				log::error!(target = "VRF", "TODO: VRF signing failed {e}!");
+				None
+			},
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::vrf::generate_vrf;
+	use super::*;
+	use runtime_utils::ExtBuilder;
+	use sp_consensus_vrf::schnorrkel::PublicKey;
+	use sp_core::blake2_256;
+
+	struct PublicData {
+		key_type_id: KeyTypeId,
+		pre_hash: H256,
+		epoch: u64,
+		task_id: H256,
+	}
+
+	fn mocked_public_data() -> PublicData {
+		let key_type_id = KeyTypeId(*b"gots");
+		let pre_hash = blake2_256(b"predecessor hash").into();
+		let epoch = 1u64;
+		let task_id = blake2_256(b"task").into();
+		PublicData { key_type_id, pre_hash, epoch, task_id }
+	}
+
+	fn add_random_key(key_type_id: KeyTypeId, builder: &ExtBuilder<()>) -> Public {
+		builder
+			.keystore
+			.as_ref()
+			.expect("A keystore")
+			.sr25519_generate_new(key_type_id, None)
+			.unwrap()
+	}
+
+	#[test]
+	#[tracing_test::traced_test]
+	fn generate_vrf_output() {
+		let PublicData { key_type_id, pre_hash, epoch, task_id } = mocked_public_data();
+
+		let builder = ExtBuilder::<()>::default().with_keystore();
+		let pubkey = add_random_key(key_type_id, &builder);
+
+		builder.build_sans_config().execute_with(|| {
+			generate_vrf(key_type_id, &pubkey, pre_hash, epoch, task_id).unwrap();
+		})
+	}
+
+	#[test]
+	fn prove_vrf_output() {
+		let PublicData { key_type_id, pre_hash, epoch, task_id } = mocked_public_data();
+
+		let builder = ExtBuilder::<()>::default().with_keystore();
+		let pubkey = add_random_key(key_type_id, &builder);
+
+		builder.build_sans_config().execute_with(|| {
+			let (output, proof) =
+				generate_vrf(key_type_id, &pubkey, pre_hash, epoch, task_id).unwrap();
+
+			prove_vrf(
+				PublicKey::from_bytes(&pubkey.0).unwrap(),
+				pre_hash,
+				epoch,
+				task_id,
+				output,
+				proof,
+			)
+			.unwrap();
+		})
+	}
+
+	//inout from prove equals inout from generator
 }
