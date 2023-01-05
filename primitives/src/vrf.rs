8c51f5686f8a3cd3397563277db4b5192d93681a
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-01-05 15:30:34 -0600

    rearrange vrf visivility

--- primitives/src/vrf.rs
+++ primitives/src/vrf.rs
@@ -5,14 +5,15 @@ use sp_arithmetic::per_things::{PerThing, Perbill, Perquintill};
 use sp_runtime_interface::pass_by::PassByCodec;
 use sp_runtime_interface::runtime_interface;
 
-pub mod sortition {
+mod model {
 	use super::*;
+
 	/// S is a hyperparameter representing participation rate.
 	/// The higher the value, the higher the chances of being sampled.
 	/// R is the prover's relative stake. the output is proportional to the stake.
-	pub fn model(s: Perquintill, r: Perquintill) -> f64 {
+	pub fn model(s: Perquintill, r: Perquintill) -> Perquintill {
 		use pdf::probability_density_function;
-		probability_density_function(s.into(), r.into()).into()
+		probability_density_function(s.into(), r.into()).0
 	}
 
 	#[derive(Encode, Decode, PassByCodec)]
@@ -31,7 +32,7 @@ pub mod sortition {
 	}
 
 	#[runtime_interface]
-	pub trait Pdf {
+	trait Pdf {
 		fn probability_density_function(sample: Wrap, weight: Wrap) -> Wrap {
 			let complement = 1f64 - sample.0.to_sub_1_float();
 			let r: f64 = weight.into();
@@ -40,13 +41,19 @@ pub mod sortition {
 		}
 	}
 
-	pub fn threshold(sample_size: Perquintill, stake: u128, total_stake: u128) -> u128 {
-		let ratio = Perquintill::from_rational(stake, total_stake);
-		let level = model(sample_size, ratio);
-		let level = BigRational::from_float(level).expect("Model codomain [0,1)");
-		let threshold = u128::MAX / level.denom() * level.numer();
-		threshold.to_u128().expect("n * X, X ~ [0,1), n: u128; qed.")
+	// Convert fixed point to f64, Accuracy depends on [PerThing::Inner]
+	pub(super) trait ToFloat: PerThing {
+		fn to_sub_1_float(&self) -> f64
+		where
+			<Self as PerThing>::Inner: Into<u64>,
+		{
+			let c = self.deconstruct();
+			c.into() as f64 / Self::ACCURACY.into() as f64
+		}
 	}
+
+	impl ToFloat for Perquintill {}
+	impl ToFloat for Perbill {}
 
 	#[cfg(test)]
 	mod tests {
@@ -57,14 +64,7 @@ pub mod sortition {
 			let r = Perquintill::from_float(0.000_001);
 			let c = Perquintill::from_float(0.5);
 
-			assert!((sortition::model(c, r) - 6.931_469_403_334_939E-7).abs() < f64::EPSILON);
-		}
-
-		#[test]
-		fn threshold_works() {
-			let x = sortition::threshold(Perquintill::from_float(0.5), 1, 1);
-			let y = u128::MAX / 2;
-			assert_eq!(x, y)
+			assert!((model(c, r).to_sub_1_float() - 6.931_469_403_334_939E-7).abs() < f64::EPSILON);
 		}
 
 		#[test]
@@ -85,16 +85,27 @@ pub mod sortition {
 	}
 }
 
-// Convert fixed point to f64, Accuracy depends on [PerThing::Inner]
-trait ToFloat: PerThing {
-	fn to_sub_1_float(&self) -> f64
-	where
-		<Self as PerThing>::Inner: Into<u64>,
-	{
-		let c = self.deconstruct();
-		c.into() as f64 / Self::ACCURACY.into() as f64
+pub mod sortition {
+	use super::*;
+	use model::ToFloat;
+
+	pub fn threshold(sample_size: Perquintill, stake: u128, total_stake: u128) -> u128 {
+		let ratio = Perquintill::from_rational(stake, total_stake);
+		let level = model::model(sample_size, ratio).to_sub_1_float();
+		let level = BigRational::from_float(level).expect("Model codomain [0,1)");
+		let threshold = u128::MAX / level.denom() * level.numer();
+		threshold.to_u128().expect("n * X, X ~ [0,1), n: u128; qed.")
+	}
+
+	#[cfg(test)]
+	mod tests {
+		use super::*;
+
+		#[test]
+		fn threshold_works() {
+			let x = sortition::threshold(Perquintill::from_float(0.5), 1, 1);
+			let y = u128::MAX / 2;
+			assert_eq!(x, y)
+		}
 	}
 }
-
-impl ToFloat for Perquintill {}
-impl ToFloat for Perbill {}
