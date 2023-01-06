7e5e25b9850b2fb113a5bad1611c297fd826e892
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-01-06 11:14:44 -0600

    Update runtime tests

--- runtime/src/tests.rs
+++ runtime/src/tests.rs
@@ -1,10 +1,11 @@
 use super::*;
 use crate::{Runtime, RuntimeCall as Call, RuntimeEvent as Event, System};
 use assert_matches::assert_matches;
-use frame_support::{assert_noop, assert_ok, traits::Hooks};
+use frame_support::{assert_noop, assert_ok};
 use frame_system::EventRecord;
 use frame_system::RawOrigin;
 use pallet_scheduler::Event as SchedulerEvent;
+use runtime_utils::{roll_to, ExtBuilder, Trivial};
 use sp_core::Pair;
 use sp_runtime::{
 	traits::{BadOrigin, IdentifyAccount},
@@ -20,37 +21,17 @@ fn generate_account(seed: &str) -> AccountId32 {
 	signer.into_account()
 }
 
-struct ExtBuilder;
-impl ExtBuilder {
-	pub fn build() -> sp_io::TestExternalities {
-		frame_system::GenesisConfig::default()
-			.build_storage::<Runtime>()
-			.unwrap()
-			.into()
-	}
-}
-
-fn scheduler_roll_to(n: BlockNumber) {
-	let now = System::block_number();
-	for i in now + 1..=n {
-		System::set_block_number(i);
-		Scheduler::on_initialize(i);
-		Scheduler::on_finalize(i);
-	}
-}
-
 #[test]
 fn pallet_scheduler_works() {
-	let mut ext = ExtBuilder::build();
-	ext.execute_with(|| {
-		System::set_block_number(1);
+	ExtBuilder::<()>::default().build_sans_config().execute_with(|| {
+		roll_to::<Trivial, Runtime, Scheduler>(1);
 		let call =
 			Call::System(frame_system::Call::remark_with_event { remark: b"dummy".to_vec() });
 		{
 			let boxed = Box::new(call);
 			assert_ok!(Scheduler::schedule(RawOrigin::Root.into(), 4, None, 0, boxed));
 		}
-		scheduler_roll_to(3);
+		roll_to::<Trivial, Runtime, Scheduler>(3);
 		assert_matches!(
 			System::events().pop().expect("Scheduled Event"),
 			EventRecord {
@@ -58,7 +39,7 @@ fn pallet_scheduler_works() {
 				..
 			}
 		);
-		scheduler_roll_to(4);
+		roll_to::<Trivial, Runtime, Scheduler>(4);
 		assert_matches!(
 			System::events().pop().expect("Dispatched Event"),
 			EventRecord {
@@ -66,16 +47,15 @@ fn pallet_scheduler_works() {
 				..
 			}
 		);
-		scheduler_roll_to(5);
+		roll_to::<Trivial, Runtime, Scheduler>(5);
 		assert_eq!(System::events().len(), 2);
 	});
 }
 
 #[test]
 fn must_be_root_to_schedule() {
-	let mut ext = ExtBuilder::build();
-	ext.execute_with(|| {
-		System::set_block_number(1);
+	ExtBuilder::<()>::default().build_sans_config().execute_with(|| {
+		roll_to::<Trivial, Runtime, Scheduler>(1);
 		let call =
 			Call::System(frame_system::Call::remark_with_event { remark: b"dummy".to_vec() });
 		let boxed = Box::new(call.clone());
