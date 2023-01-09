42eff225bda7dbdeed64eceb4f5cdc047362e0bc
Nathan Whitaker <nathan.whitaker01@gmail.com>
2023-01-09 14:08:08 -0800

    Use mimalloc

--- node/src/main.rs
+++ node/src/main.rs
@@ -9,6 +9,11 @@ mod cli;
 mod command;
 mod rpc;
 
+use mimalloc_rust::GlobalMiMalloc;
+
+#[global_allocator]
+static ALLOCATOR: GlobalMiMalloc = GlobalMiMalloc;
+
 fn main() -> sc_cli::Result<()> {
 	command::run()
 }
