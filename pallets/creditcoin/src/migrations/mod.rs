62d9d65081b4c45ca7948eb1d449ef3e859a42a3
Alex Todorov <alex.todorov@gluwa.com>
2023-01-16 15:00:35 +0000

    Redesign how pre_upgrade()/post_upgrade() functions are called
    these are no longer Substrate hooks but rather a place where we can
    perform more assertions while testing migrations in CI.
    
    scripts/check-storage-version-assert-in-migrations.sh now becomes
    useless.

--- pallets/creditcoin/src/migrations/mod.rs
+++ pallets/creditcoin/src/migrations/mod.rs
@@ -9,55 +9,79 @@ mod v5;
 mod v6;
 mod v7;
 
-#[cfg(feature = "try-runtime")]
-pub(crate) fn pre_upgrade<T: Config>(version: StorageVersion) -> Result<(), &'static str> {
-	// potentially do some checks on the state before the migration
-	if version < 6 {
-		v6::pre_upgrade::<T>()?;
-	}
-	Ok(())
-}
-
-#[cfg(feature = "try-runtime")]
-pub(crate) fn post_upgrade<T: Config>(version: StorageVersion) -> Result<(), &'static str> {
-	// do some post-migration checks
-	if version < 6 {
-		v6::post_upgrade::<T>()?;
-	}
-	Ok(())
-}
-
 pub(crate) fn migrate<T: Config>() -> Weight {
 	let version = StorageVersion::get::<Pallet<T>>();
 	let mut weight: Weight = Weight::zero();
 
 	if version < 1 {
+		#[cfg(feature = "try-runtime")]
+		v1::pre_upgrade::<T>();
+
 		weight.saturating_accrue(v1::migrate::<T>());
 		StorageVersion::new(1).put::<Pallet<T>>();
+
+		#[cfg(feature = "try-runtime")]
+		v1::post_upgrade::<T>();
 	}
 	if version < 2 {
+		#[cfg(feature = "try-runtime")]
+		v2::pre_upgrade::<T>();
+
 		weight.saturating_accrue(v2::migrate::<T>());
 		StorageVersion::new(2).put::<Pallet<T>>();
+
+		#[cfg(feature = "try-runtime")]
+		v2::post_upgrade::<T>();
 	}
 	if version < 3 {
+		#[cfg(feature = "try-runtime")]
+		v3::pre_upgrade::<T>();
+
 		weight.saturating_accrue(v3::migrate::<T>());
 		StorageVersion::new(3).put::<Pallet<T>>();
+
+		#[cfg(feature = "try-runtime")]
+		v3::post_upgrade::<T>();
 	}
 	if version < 4 {
+		#[cfg(feature = "try-runtime")]
+		v4::pre_upgrade::<T>();
+
 		weight.saturating_accrue(v4::migrate::<T>());
 		StorageVersion::new(4).put::<Pallet<T>>();
+
+		#[cfg(feature = "try-runtime")]
+		v4::post_upgrade::<T>();
 	}
 	if version < 5 {
+		#[cfg(feature = "try-runtime")]
+		v5::pre_upgrade::<T>();
+
 		weight.saturating_accrue(v5::migrate::<T>());
 		StorageVersion::new(5).put::<Pallet<T>>();
+
+		#[cfg(feature = "try-runtime")]
+		v5::post_upgrade::<T>();
 	}
 	if version < 6 {
+		#[cfg(feature = "try-runtime")]
+		v6::pre_upgrade::<T>();
+
 		weight.saturating_accrue(v6::migrate::<T>());
 		StorageVersion::new(6).put::<Pallet<T>>();
+
+		#[cfg(feature = "try-runtime")]
+		v6::post_upgrade::<T>();
 	}
 	if version < 7 {
+		#[cfg(feature = "try-runtime")]
+		v7::pre_upgrade::<T>();
+
 		weight.saturating_accrue(v7::migrate::<T>());
 		StorageVersion::new(7).put::<Pallet<T>>();
+
+		#[cfg(feature = "try-runtime")]
+		v7::post_upgrade::<T>();
 	}
 
 	weight
