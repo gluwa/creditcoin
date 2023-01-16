142d3d4945d70bf19c16c03d22f7caa3590e0a2b
Alex Todorov <alex.todorov@gluwa.com>
2023-01-16 18:07:49 +0000

    Update migrate() to use a loop & a map of callbacks
    makes the code more concise and easier to read and removes the explicit
    checks for version number before applying the next migration!

--- pallets/creditcoin/src/migrations/mod.rs
+++ pallets/creditcoin/src/migrations/mod.rs
@@ -1,5 +1,6 @@
 use crate::{Config, Pallet};
 use frame_support::{traits::StorageVersion, weights::Weight};
+use std::collections::HashMap;
 
 mod v1;
 mod v2;
@@ -13,75 +14,29 @@ pub(crate) fn migrate<T: Config>() -> Weight {
 	let version = StorageVersion::get::<Pallet<T>>();
 	let mut weight: Weight = Weight::zero();
 
-	if version < 1 {
-		#[cfg(feature = "try-runtime")]
-		v1::pre_upgrade::<T>();
+	let callbacks = HashMap::from([
+		(1, (v1::pre_upgrade::<T>, v1::migrate::<T>, v1::post_upgrade::<T>)),
+		(2, (v2::pre_upgrade::<T>, v2::migrate::<T>, v2::post_upgrade::<T>)),
+		(3, (v3::pre_upgrade::<T>, v3::migrate::<T>, v3::post_upgrade::<T>)),
+		(4, (v4::pre_upgrade::<T>, v4::migrate::<T>, v4::post_upgrade::<T>)),
+		(5, (v5::pre_upgrade::<T>, v5::migrate::<T>, v5::post_upgrade::<T>)),
+		(6, (v6::pre_upgrade::<T>, v6::migrate::<T>, v6::post_upgrade::<T>)),
+		(7, (v7::pre_upgrade::<T>, v7::migrate::<T>, v7::post_upgrade::<T>)),
+	]);
 
-		weight.saturating_accrue(v1::migrate::<T>());
-		StorageVersion::new(1).put::<Pallet<T>>();
-
-		#[cfg(feature = "try-runtime")]
-		v1::post_upgrade::<T>();
-	}
-	if version < 2 {
-		#[cfg(feature = "try-runtime")]
-		v2::pre_upgrade::<T>();
-
-		weight.saturating_accrue(v2::migrate::<T>());
-		StorageVersion::new(2).put::<Pallet<T>>();
+	for migration_number in 1 + version.into()..=callbacks.keys().len() {
+		let (pre_hook, migrate_hook, post_hook) = callbacks
+			.get(migration_number)
+			.expect("No callbacks found for version {version:?}");
 
 		#[cfg(feature = "try-runtime")]
-		v2::post_upgrade::<T>();
-	}
-	if version < 3 {
-		#[cfg(feature = "try-runtime")]
-		v3::pre_upgrade::<T>();
+		pre_hook();
 
-		weight.saturating_accrue(v3::migrate::<T>());
-		StorageVersion::new(3).put::<Pallet<T>>();
+		weight.saturating_accrue(migrate_hook());
+		StorageVersion::new(migration_number).put::<Pallet<T>>();
 
 		#[cfg(feature = "try-runtime")]
-		v3::post_upgrade::<T>();
-	}
-	if version < 4 {
-		#[cfg(feature = "try-runtime")]
-		v4::pre_upgrade::<T>();
-
-		weight.saturating_accrue(v4::migrate::<T>());
-		StorageVersion::new(4).put::<Pallet<T>>();
-
-		#[cfg(feature = "try-runtime")]
-		v4::post_upgrade::<T>();
-	}
-	if version < 5 {
-		#[cfg(feature = "try-runtime")]
-		v5::pre_upgrade::<T>();
-
-		weight.saturating_accrue(v5::migrate::<T>());
-		StorageVersion::new(5).put::<Pallet<T>>();
-
-		#[cfg(feature = "try-runtime")]
-		v5::post_upgrade::<T>();
-	}
-	if version < 6 {
-		#[cfg(feature = "try-runtime")]
-		v6::pre_upgrade::<T>();
-
-		weight.saturating_accrue(v6::migrate::<T>());
-		StorageVersion::new(6).put::<Pallet<T>>();
-
-		#[cfg(feature = "try-runtime")]
-		v6::post_upgrade::<T>();
-	}
-	if version < 7 {
-		#[cfg(feature = "try-runtime")]
-		v7::pre_upgrade::<T>();
-
-		weight.saturating_accrue(v7::migrate::<T>());
-		StorageVersion::new(7).put::<Pallet<T>>();
-
-		#[cfg(feature = "try-runtime")]
-		v7::post_upgrade::<T>();
+		post_hook();
 	}
 
 	weight
