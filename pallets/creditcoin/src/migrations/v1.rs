01612c19b5df8aae24e2cb725da456daa83629b0
Alex Todorov <alex.todorov@gluwa.com>
2023-01-16 15:00:35 +0000

    Redesign how pre_upgrade()/post_upgrade() functions are called
    these are no longer Substrate hooks but rather a place where we can
    perform more assertions while testing migrations in CI.
    
    scripts/check-storage-version-assert-in-migrations.sh now becomes
    useless.

--- pallets/creditcoin/src/migrations/v1.rs
+++ pallets/creditcoin/src/migrations/v1.rs
@@ -462,11 +462,13 @@ mod tests {
 }
 
 #[cfg(feature = "try-runtime")]
-pub(crate) fn post_upgrade<T: Config>() -> Result<(), &'static str> {
-	ensure!(
-		StorageVersion::get::<crate::Pallet<T>>() == 1,
+pub(crate) fn pre_upgrade<T: Config>() {}
+
+#[cfg(feature = "try-runtime")]
+pub(crate) fn post_upgrade<T: Config>() {
+	assert_eq!(
+		StorageVersion::get::<crate::Pallet<T>>(),
+		1,
 		"expected storage version to be 1 after migrations complete"
 	);
-
-	Ok(())
 }
