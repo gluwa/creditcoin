c3589e57fcdbb908c230e255f3dc28b301600d23
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-01-16 22:37:06 -0600

    staking/unstaking test

--- runtime/src/tests/staking.rs
+++ runtime/src/tests/staking.rs
@@ -1,29 +1,47 @@
 use super::generate_account;
-use crate::mock::runtime::{AccountId, Balances, BondingDuration, Runtime, RuntimeOrigin, Session};
+use crate::mock::runtime::{
+	AccountId, Balances, BlocksPerEra, BondingDuration, Runtime, RuntimeOrigin, Session,
+};
 use frame_support::assert_ok;
 use frame_support::traits::Currency;
 use pallet_staking::RewardDestination;
+use pallet_staking::StakingInterface;
 use runtime_utils::{ExtBuilder, RollTo, Trivial};
 use std::default::Default;
 
 type Staking = pallet_staking::Pallet<Runtime>;
 
+const BLOCKS_UNTIL_UNBONDED: u32 = BlocksPerEra::get() * BondingDuration::get();
+
 #[test]
-// test with different payees
-fn stash_bonds_with_controller_to_stash() {
+fn active_era_transitions() {
+	ExtBuilder::<()>::default().build_sans_config().execute_with(|| {
+		assert!(Staking::active_era().is_none());
+		let mut height = 1;
+		Trivial::<Session, Runtime>::roll_to(height);
+		assert_eq!(2, Staking::eras_start_session_index(0).unwrap());
+		height += BlocksPerEra::get();
+		Trivial::<Session, Runtime>::roll_to(height + 1);
+		assert_eq!(1, Staking::active_era().unwrap().index);
+	});
+}
+
+#[test]
+fn stake_and_unstake_with_a_controller_and_stash_as_payee() {
 	let stash = generate_account("stash");
 	let controller = generate_account("controller");
 	let value = <Balances as Currency<AccountId>>::minimum_balance();
 
 	ExtBuilder::<()>::default().build_sans_config().execute_with(|| {
-		Trivial::<(Session, Runtime)>::roll_to(1);
+		let mut height = 1;
+		Trivial::<Session, Runtime>::roll_to(height);
 
-		let _ = Balances::deposit_creating(&stash, 2 * value);
-
+		// prime balance to create the account
+		let _ = Balances::deposit_creating(&stash, value);
 		let usable = Balances::usable_balance(&stash);
-		assert_eq!(2 * value, usable);
+		assert_eq!(value, usable);
 
-		//in what era
+		// lock stash's funds under a controller
 		assert_ok!(Staking::bond(
 			RuntimeOrigin::signed(stash.clone()),
 			controller.clone().into(),
@@ -31,26 +49,31 @@ fn stash_bonds_with_controller_to_stash() {
 			RewardDestination::Stash,
 		));
 
+		// stake is found and usable balance dropped
+		assert_eq!(value, Staking::total_stake(&controller).unwrap());
 		let usable = Balances::usable_balance(&stash);
-		assert_eq!(value, usable);
+		assert_eq!(0, usable);
 
-		//unbond
+		// unbond, efective after BondingPeriod has passed
 		assert_ok!(Staking::unbond(RuntimeOrigin::signed(controller.clone()), value));
+		assert_eq!(value, Staking::total_stake(&controller).unwrap());
+		let usable = Balances::usable_balance(&stash);
+		assert_eq!(0, usable);
 
-		//unbondig defered until bonding eras pass
+		// Cant withdraw before minimum staking period has elapsed; NOOPs
+		assert_ok!(Staking::withdraw_unbonded(RuntimeOrigin::signed(controller.clone()), 0));
+		assert_eq!(value, Staking::total_stake(&controller).unwrap());
+		let usable = Balances::usable_balance(&stash);
+		assert_eq!(0, usable);
+
+		// unbonding defered until bonding eras pass
+		height += BLOCKS_UNTIL_UNBONDED;
+
+		Trivial::<Session, Runtime>::roll_to(height);
+
+		assert_ok!(Staking::withdraw_unbonded(RuntimeOrigin::signed(controller), 0));
 		let usable = Balances::usable_balance(&stash);
 		assert_eq!(value, usable);
-
-		// eras pass
-		Trivial::<(Session, Runtime)>::roll_to(BondingDuration::get() + 1);
-		//TODO end_era
-
-		//unbondig defered until bonding eras pass
-		let usable = Balances::usable_balance(&stash);
-		assert_eq!(2 * value, usable);
+		assert!(Staking::total_stake(&stash).is_none());
 	});
-
-	//assert on balances
 }
-
-//only the controller can unbond
