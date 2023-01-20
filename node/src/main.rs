7131fb6954dc48edc697bf649fbf4a5d55828bff
Nathan Whitaker <nathan.whitaker01@gmail.com>
2023-01-20 09:39:00 -0800

    Revert "Use mimalloc"
    This reverts commit 42eff225bda7dbdeed64eceb4f5cdc047362e0bc.

--- node/src/main.rs
+++ node/src/main.rs
@@ -9,11 +9,6 @@ mod cli;
 mod command;
 mod rpc;
 
-use mimalloc_rust::GlobalMiMalloc;
-
-#[global_allocator]
-static ALLOCATOR: GlobalMiMalloc = GlobalMiMalloc;
-
 fn main() -> sc_cli::Result<()> {
 	command::run()
 }
