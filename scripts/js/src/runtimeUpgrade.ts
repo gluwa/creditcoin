35451079f32576a2625b312f309d105975acd374
Nathan Whitaker <nathan.whitaker01@gmail.com>
2023-01-30 12:54:48 -0800

    Dynamically determine correct Weight type

--- scripts/js/src/runtimeUpgrade.ts
+++ scripts/js/src/runtimeUpgrade.ts
@@ -1,5 +1,5 @@
-import { creditcoinApi } from 'creditcoin-js';
-import { Keyring } from '@polkadot/api';
+import { BN, creditcoinApi } from 'creditcoin-js';
+import { ApiPromise, Keyring } from '@polkadot/api';
 import * as fs from 'fs';
 import * as child_process from 'child_process';
 import { promisify } from 'util';
@@ -27,6 +27,36 @@ type WasmRuntimeInfo = {
 // these normally use callbacks, but promises are more convenient
 const readFile = promisify(fs.readFile);
 const exec = promisify(child_process.exec);
+
+type OldWeight = BN;
+type NewWeight = { refTime: BN; proofSize: BN };
+type Weight = OldWeight | NewWeight;
+
+function createOverrideWeight(api: ApiPromise): Weight {
+    const sudoCallTypeId = api.runtimeMetadata.registry.metadata.pallets
+        .find((v) => v.name.toString() == 'Sudo')
+        .calls.unwrap().type;
+    const sudoCallTypeDef = api.runtimeMetadata.registry.lookup.getTypeDef(sudoCallTypeId);
+    if (Array.isArray(sudoCallTypeDef.sub)) {
+        const sudoUncheckedWeightType = sudoCallTypeDef.sub.find((def) => def.name == 'sudo_unchecked_weight');
+        if (Array.isArray(sudoUncheckedWeightType.sub)) {
+            // the weight is the second argument to sudo_unchecked_weight
+            const weightType = sudoUncheckedWeightType.sub[1];
+            if (weightType.type == 'u64') {
+                // old weight (simple u64)
+                return new BN(1);
+            } else {
+                // new weight
+                return {
+                    refTime: new BN(1),
+                    proofSize: new BN(0),
+                };
+            }
+        }
+    }
+
+    throw new Error("Couldn't find expected Weight type from sudoUncheckedWeight metadata");
+}
 
 /**
  * Performs an upgrade to the runtime at the provided path.
@@ -89,11 +119,11 @@ async function doRuntimeUpgrade(
         if (scheduleDelay > 0) {
             callback = api.tx.scheduler.scheduleAfter(scheduleDelay, null, 0, callback);
         }
-
+        const overrideWeight = createOverrideWeight(api);
         // schedule the upgrade
         await new Promise<void>((resolve, reject) => {
             const unsubscribe = api.tx.sudo
-                .sudoUncheckedWeight(callback, { refTime: 1, proofSize: 0 })
+                .sudoUncheckedWeight(callback, overrideWeight)
                 .signAndSend(keyring, { nonce: -1 }, (result) => {
                     const finish = (fn: () => void) => {
                         unsubscribe
