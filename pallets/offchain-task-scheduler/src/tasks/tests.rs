560dfd9a46bb7fb820ab170cf995a9364826c56f
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-02-27 19:32:39 -0600

    CSUB-437move lock tests

--- pallets/offchain-task-scheduler/src/tasks/tests.rs
+++ pallets/offchain-task-scheduler/src/tasks/tests.rs
@@ -1,16 +1,18 @@
 #![cfg(test)]
 
 use super::StorageLock;
-use crate::mock::{ExtBuilder, Test};
+use crate::mock::runtime::Runtime;
 use frame_system::Pallet as System;
+use runtime_utils::ExtBuilder;
 use sp_core::offchain::Duration;
 use sp_io::offchain;
 use sp_runtime::offchain::storage_lock::{BlockAndTime, Time};
 
 #[test]
 fn lock_released_when_guard_is_dropped() {
-	let ext = ExtBuilder::default();
-	ext.build_offchain_and_execute_with_state(|state, _| {
+	let mut ext = ExtBuilder::default();
+	let state = ext.with_offchain();
+	ext.build_sans_config().execute_with(|| {
 		let key = b"id_1";
 		let mut l1 = StorageLock::<'_, Time>::new(key);
 		let g = l1.try_lock();
@@ -22,8 +24,9 @@ fn lock_released_when_guard_is_dropped() {
 
 #[test]
 fn lock_guard_is_kept_alive() {
-	let ext = ExtBuilder::default();
-	ext.build_offchain_and_execute_with_state(|_, _| {
+	let mut ext = ExtBuilder::default();
+	ext.with_offchain();
+	ext.build_sans_config().execute_with(|| {
 		let mut l1 = StorageLock::<'_, Time>::new(b"id_1");
 		let g = l1.try_lock();
 		g.expect("ok").forget();
@@ -34,17 +37,18 @@ fn lock_guard_is_kept_alive() {
 
 #[test]
 fn lock_expires() {
-	let ext = ExtBuilder::default();
-	ext.build_offchain_and_execute_with_state(|_, _| {
-		System::<Test>::set_block_number(1);
-		let mut l1 = StorageLock::<'_, BlockAndTime<System<Test>>>::with_block_and_time_deadline(
+	let mut ext = ExtBuilder::default();
+	ext.with_offchain();
+	ext.build_sans_config().execute_with(|| {
+		System::<Runtime>::set_block_number(1);
+		let mut l1 = StorageLock::<'_, BlockAndTime<System<Runtime>>>::with_block_and_time_deadline(
 			b"id_1",
 			1,
 			Duration::from_millis(0),
 		);
 		let g = l1.try_lock().expect("ok");
 		g.forget();
-		System::<Test>::set_block_number(3);
+		System::<Runtime>::set_block_number(3);
 		let sleep_until = offchain::timestamp().add(Duration::from_millis(1));
 		offchain::sleep_until(sleep_until);
 		let g = l1.try_lock();
@@ -54,8 +58,9 @@ fn lock_expires() {
 
 #[test]
 fn lock_mutual_exclusion() {
-	let ext = ExtBuilder::default();
-	ext.build_offchain_and_execute_with_state(|state, _| {
+	let mut ext = ExtBuilder::default();
+	let state = ext.with_offchain();
+	ext.build_sans_config().execute_with(|| {
 		let mut l1 = StorageLock::<'_, Time>::new(b"id_1");
 		let mut l2 = StorageLock::<'_, Time>::new(b"id_2");
 
