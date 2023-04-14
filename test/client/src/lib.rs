47e37d5c49331b38bfc3b050184181e33e2f0408
Nathan Whitaker <nathan.whitaker01@gmail.com>
2023-04-14 13:12:39 -0700

    Fix test client

--- test/client/src/lib.rs
+++ test/client/src/lib.rs
@@ -1,10 +1,13 @@
 use creditcoin_node_runtime::{
-	self as runtime, Block, GenesisConfig, SystemConfig as SystemGenesisConfig, WASM_BINARY,
+	self as runtime, BabeConfig, Block, GenesisConfig, SystemConfig, WASM_BINARY,
 };
+use runtime::{AccountId, BabeId, GrandpaId, ImOnlineId, SessionConfig, Signature};
 use sc_chain_spec::construct_genesis_block;
 use sc_service::client;
-use sp_core::twox_128;
-use sp_runtime::traits::{Block as BlockT, Hash as HashT, Header as HeaderT};
+use sp_core::{twox_128, Pair, Public};
+use sp_runtime::traits::{
+	Block as BlockT, Hash as HashT, Header as HeaderT, IdentifyAccount, Verify,
+};
 use sp_runtime::Storage;
 use std::collections::BTreeMap;
 
@@ -51,10 +54,54 @@ pub struct GenesisParameters {
 	wasm_code: Option<Vec<u8>>,
 }
 
+/// Generate a crypto pair from seed.
+pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
+	TPublic::Pair::from_string(&format!("//{seed}"), None)
+		.expect("static values are valid; qed")
+		.public()
+}
+
+type AccountPublic = <Signature as Verify>::Signer;
+
+/// Generate an account ID from seed.
+pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId
+where
+	AccountPublic: From<<TPublic::Pair as Pair>::Public>,
+{
+	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
+}
+
+type AuthorityKeys = (AccountId, AccountId, runtime::SessionKeys);
+
+pub fn get_authority_keys_from_seed(seed: &str) -> AuthorityKeys {
+	(
+		get_account_id_from_seed::<sp_core::sr25519::Public>(&format!("{}//stash", seed)),
+		get_account_id_from_seed::<sp_core::sr25519::Public>(&format!("{}//stash", seed)),
+		make_session_keys(
+			get_from_seed::<GrandpaId>(seed),
+			get_from_seed::<BabeId>(seed),
+			get_from_seed::<ImOnlineId>(seed),
+		),
+	)
+}
+
+fn make_session_keys(
+	grandpa: GrandpaId,
+	babe: BabeId,
+	im_online: ImOnlineId,
+) -> creditcoin_node_runtime::SessionKeys {
+	creditcoin_node_runtime::SessionKeys { grandpa, babe, im_online }
+}
+
 impl GenesisParameters {
 	fn genesis_config(&self) -> GenesisConfig {
 		GenesisConfig {
-			system: SystemGenesisConfig { code: WASM_BINARY.expect("WASM_BUILD").to_vec() },
+			system: SystemConfig { code: WASM_BINARY.expect("WASM_BUILD").to_vec() },
+			babe: BabeConfig {
+				authorities: vec![],
+				epoch_config: Some(runtime::BABE_GENESIS_EPOCH_CONFIG),
+			},
+			session: SessionConfig { keys: vec![get_authority_keys_from_seed("//Alice")] },
 			..Default::default()
 		}
 	}
