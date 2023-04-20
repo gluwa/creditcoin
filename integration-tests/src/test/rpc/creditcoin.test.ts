285f236952c3e14ed768ba414eef4217e12d0980
Alex Todorov <alex.todorov@gluwa.com>
2023-04-20 13:29:13 +0000

    Remove integration test for creditcoin.hashrate RPC
    and adjust the asserts in Prometheus metrics test.
    
    the PoS consensus doesn't require any mining, so we don't produce
    any hashes and the entire hashrate metric & RPC are now useless
    and should be removed.

--- integration-tests/src/test/rpc/creditcoin.test.ts
+++ integration-tests/src/test/rpc/creditcoin.test.ts
@@ -1,4 +1,4 @@
-// Copyright 2022 Gluwa, Inc. & contributors
+// Copyright 2022-2023 Gluwa, Inc. & contributors
 // SPDX-License-Identifier: The Unlicense
 
 import { WebSocket } from 'ws';
@@ -19,13 +19,6 @@ describe('Creditcoin RPC', (): void => {
         api = await ApiPromise.create({
             provider,
             rpc: {
-                creditcoin: {
-                    hashrate: {
-                        params: [],
-                        description: 'Get hashrate',
-                        type: 'Json',
-                    },
-                },
                 task: {
                     getOffchainNonceKey: {
                         params: [
@@ -44,35 +37,6 @@ describe('Creditcoin RPC', (): void => {
 
     afterAll(async () => {
         await api.disconnect();
-    });
-
-    it.skip('hashrate() should return a valid hashrate', async () => {
-        type HashrateStats = {
-            elapsed: Elapsed;
-            hash_count: number; // eslint-disable-line
-            rate: number;
-        };
-
-        type Elapsed = {
-            nanos: number;
-            secs: number;
-        };
-        const rate = await (api.rpc as any).creditcoin.hashrate();
-        const rateObj: HashrateStats = JSON.parse(rate);
-
-        const shortName: string = (global as any).CREDITCOIN_NETWORK_SHORT_NAME;
-
-        // the nodes dedicated to serving RPCs don't mine blocks
-        // and therefore don't produce any hashes
-        if (shortName === 'creditcoin_testnet') {
-            expect(rateObj.rate).toBe(0);
-            expect(rateObj.hash_count).toBe(0); // eslint-disable-line
-        } else {
-            expect(rateObj.rate).toBeGreaterThan(0);
-            expect(rateObj.hash_count).toBeGreaterThan(0); // eslint-disable-line
-        }
-        expect(rateObj.elapsed.secs).toBeGreaterThan(0);
-        expect(rateObj.elapsed.nanos).toBeGreaterThan(0);
     });
 
     it('getOffchainNonceKey() should return error when input is not a valid hex string', (done): void => {
