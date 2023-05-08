8192fe8dedb75b02ea8985e84b932df52f2eb5f7
Alex Todorov <alex.todorov@gluwa.com>
2023-05-08 11:58:55 +0000

    Adjust expected values in devnet test config

--- integration-tests/src/devnetSetup.ts
+++ integration-tests/src/devnetSetup.ts
@@ -32,8 +32,8 @@ const setup = async () => {
     (global as any).CREDITCOIN_ETHEREUM_USE_HARDHAT_WALLET = false;
 
     (global as any).CREDITCOIN_EXECUTE_SETUP_AUTHORITY = false;
-    (global as any).CREDITCOIN_NETWORK_LONG_NAME = 'Creditcoin Testnet';
-    (global as any).CREDITCOIN_NETWORK_SHORT_NAME = 'creditcoin_testnet';
+    (global as any).CREDITCOIN_NETWORK_LONG_NAME = 'Creditcoin Dev';
+    (global as any).CREDITCOIN_NETWORK_SHORT_NAME = 'creditcoin_dev';
     (global as any).CREDITCOIN_METRICS_BASE = 'http://dev-rpc-creditcoin-rpc-2.centralus.cloudapp.azure.com:9615';
     (global as any).CREDITCOIN_REUSE_EXISTING_ADDRESSES = true;
 
