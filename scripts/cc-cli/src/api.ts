bdbcc5fb3859a5da862a7140fd77413d17a6585e
Pablo Labarta <pablitolabarta@gmail.com>
2023-05-16 13:05:14 -0300

    clean newApi func

--- scripts/cc-cli/src/api.ts
+++ scripts/cc-cli/src/api.ts
@@ -1,11 +1,8 @@
 import { ApiPromise, WsProvider } from "creditcoin-js";
 import { cryptoWaitReady } from "@polkadot/util-crypto";
+
 // Create new API instance
-export async function newApi(url: string) {
-  if (!url) {
-    url = "ws://localhost:9944";
-  }
-  // console.log("Connecting to node at:", url);
+export async function newApi(url: string = "ws://localhost:9944") {
   const api = await ApiPromise.create({
     provider: new WsProvider(url),
   });
