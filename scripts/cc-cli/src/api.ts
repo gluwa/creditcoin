f6d65f92932a876e9c64294b5c8aa51bb12e74a4
Pablo Labarta <pablitolabarta@gmail.com>
2023-05-17 11:15:33 -0300

    remove type annotation

--- scripts/cc-cli/src/api.ts
+++ scripts/cc-cli/src/api.ts
@@ -2,7 +2,7 @@ import { ApiPromise, WsProvider } from "creditcoin-js";
 import { cryptoWaitReady } from "@polkadot/util-crypto";
 
 // Create new API instance
-export async function newApi(url: string = "ws://localhost:9944") {
+export async function newApi(url = "ws://localhost:9944") {
   const api = await ApiPromise.create({
     provider: new WsProvider(url),
   });
