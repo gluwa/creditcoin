c6bd1b384ffbf1b70f801fe6d7af6e840395fa53
Pablo Labarta <pablitolabarta@gmail.com>
2023-06-19 16:29:51 -0300

    add: change confirmations to using prompts

--- scripts/cc-cli/src/utils/promptContinue.ts
+++ scripts/cc-cli/src/utils/promptContinue.ts
@@ -1,36 +1,27 @@
-import readline from "readline";
-
-function askQuestion(query: string): Promise<string> {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  return new Promise<string>((resolve) =>
-    rl.question(query, (ans) => {
-      rl.close();
-      resolve(ans);
-    })
-  );
-}
+import prompts from "prompts";
 
 export async function promptContinue() {
-  const answer = await askQuestion("Continue? (y/n): ");
-  if (answer.toLowerCase() !== "y") {
-    console.log("Aborting...");
-    process.exit(0);
-  }
+  const promptResult = await prompts({
+    type: "confirm",
+    name: "confirm",
+    message: "Continue?",
+    initial: true,
+  });
+
+  return promptResult.confirm;
 }
 
 export async function promptContinueOrSkip(prompt: string) {
-  let answer = await askQuestion(`${prompt} (y/skip): `);
-  while (answer !== "y" && answer !== "skip") {
-    console.log("Invalid input");
-    answer = await askQuestion(`${prompt} (y/skip): `);
-  }
-  if (answer === "skip") {
-    return false;
-  } else if (answer === "y") {
-    return true;
-  }
+  const promptResult = await prompts({
+    type: "select",
+    name: "continue",
+    message: prompt,
+    choices: [
+      { title: "Continue", value: true },
+      { title: "Skip", value: false },
+    ],
+    initial: 1,
+  });
+
+  return promptResult.continue;
 }
