2083df78e438167434c7fdfe0fc725c43ba4746e
Pablo Labarta <pablitolabarta@gmail.com>
2023-07-03 10:56:35 -0300

    add: new parsing & tests

---
+++ scripts/cc-cli/src/utils/parsing.ts
@@ -0,0 +1,106 @@
+import { validateAddress } from "@polkadot/util-crypto/address";
+import { BN, parseUnits } from "creditcoin-js";
+
+export const parseAddresOrExit = parseOrExit(parseAddress);
+export const parseAmountOrExit = parseOrExit(parseAmount);
+export const parseHexStringOrExit = parseOrExit(parseHexString);
+export const parseIntegerOrExit = parseOrExit(parseInteger);
+export const parsePercentAsPerbillOrExit = parseOrExit(parsePercentAsPerbill);
+export const parseChoiceOrExit = parseChoiceOrExitFn;
+
+// A function that takes a parsing function and returns a new function that does tries to parse or prints the error and exits
+function parseOrExit<T>(parse: (input: string) => T): (input: string) => T {
+  return (input: string) => {
+    try {
+      return parse(input);
+    } catch (e: any) {
+      console.error(`Unable to parse input. ${e.message}`);
+      process.exit(1);
+    }
+  };
+}
+
+function parseChoiceOrExitFn(input: string, choices: string[]): string | never {
+  try {
+    return parseChoice(input, choices);
+  } catch (e: any) {
+    console.error(`Unable to parse input. ${e.message}`);
+    process.exit(1);
+  }
+}
+
+export function parseAddress(input: string): string {
+  try {
+    validateAddress(input);
+  } catch (e: any) {
+    throw new Error(`Invalid address: ${e.message}`);
+  }
+  return input;
+}
+
+export function parseAmount(input: string): BN {
+  try {
+    const parsed = positiveBigNumberFromString(input);
+    return new BN(parsed.toString());
+  } catch (e: any) {
+    throw new Error(`Invalid amount: ${e.message}`);
+  }
+}
+
+// Choices must be in Capitalized form: ['Staked', 'Stash', 'Controller']
+export function parseChoice(input: string, choices: string[]): string {
+  let styled = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+  if (!choices.includes(styled)) {
+    throw new Error(
+      `Invalid choice: ${input}, must be one of ${choices.flatMap(
+        (c) => `'${c}' `
+      )}`
+    );
+  }
+  return styled;
+}
+
+export function parseBoolean(input: true | undefined): boolean {
+  return input ? true : false;
+}
+
+export function parseInteger(input: string): number {
+  const float = Number.parseFloat(input);
+  if (float % 1 !== 0) {
+    throw new Error("Must be an integer");
+  }
+  const int = Number.parseInt(input, 10);
+  return int;
+}
+
+export function parseHexString(input: string): string {
+  if (!input.startsWith("0x")) {
+    throw new Error("Must start with 0x");
+  }
+  return input;
+}
+
+export function parsePercentAsPerbill(input: string): number {
+  if (input.match(/[^0-9.]/)) {
+    throw new Error("Percent value must be a number");
+  }
+  const value = Number.parseFloat(input);
+  if (value < 0 || value > 100) {
+    throw new Error("Percent value must be between 0 and 100");
+  }
+  return Math.floor(value * 10_000_000);
+}
+
+function positiveBigNumberFromString(amount: string) {
+  const parsedValue = parseUnits(amount, 18);
+
+  if (parsedValue.isZero()) {
+    throw new Error("Must be greater than 0");
+  }
+
+  if (parsedValue.isNegative()) {
+    throw new Error("Must be a positive number");
+  }
+
+  return parsedValue;
+}
