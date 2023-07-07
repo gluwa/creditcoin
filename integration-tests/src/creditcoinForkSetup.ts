cbd8c76d36a512ff28e46850ce9621bf7d64ff9a
Alex Todorov <alex.todorov@gluwa.com>
2023-07-07 22:48:01 +0300

    Attempt switch-to-pos.test only if network is still PoW

--- integration-tests/src/creditcoinForkSetup.ts
+++ integration-tests/src/creditcoinForkSetup.ts
@@ -8,6 +8,8 @@ const setup = async () => {
     // to satisfy the regexp in metrics.test.ts
     (global as any).CREDITCOIN_NETWORK_SHORT_NAME = '.*-fork';
 
+    (global as any).CREDITCOIN_SWITCH_TO_POS_ALREADY_CALLED = process.env.SWITCH_TO_POS_ALREADY_CALLED === '1';
+
     await globalSetup();
 };
 
