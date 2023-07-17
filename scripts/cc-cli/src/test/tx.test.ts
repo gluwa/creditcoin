94431316c07590c2c4c7ac5a1764ffe5afc027ee
Pablo Labarta <pablitolabarta@gmail.com>
2023-07-17 16:19:28 -0300

    add: refactor & simple tests

---
+++ scripts/cc-cli/src/test/tx.test.ts
@@ -0,0 +1,54 @@
+import { BN } from "creditcoin-js";
+import { AccountBalance } from "../utils/balance";
+import { canPay } from "../utils/tx";
+
+describe("unit test: utils/tx", () => {
+  test("balance can check for negatives", () => {
+    const negOne = new BN(1).sub(new BN(2));
+    expect(negOne.toString()).toStrictEqual("-1");
+  });
+});
+
+describe("unit tests: utils/tx", () => {
+  test("given a payable amount, canPay returns true", () => {
+    const balance: AccountBalance = {
+      address: "fakeAddress",
+      total: new BN(100),
+      bonded: new BN(0),
+      unbonding: new BN(0),
+      transferable: new BN(100),
+      locked: new BN(0),
+    };
+
+    const amountUsedInTx = new BN(10);
+
+    const txFee = new BN(1);
+
+    const totalCost = amountUsedInTx.add(txFee);
+
+    const canPayResult = canPay(balance, totalCost);
+
+    expect(canPayResult).toStrictEqual(true);
+  });
+
+  test("given an unpayable amount, canPay returns false", () => {
+    const balance: AccountBalance = {
+      address: "fakeAddress",
+      total: new BN(100),
+      bonded: new BN(0),
+      unbonding: new BN(0),
+      transferable: new BN(100),
+      locked: new BN(0),
+    };
+
+    const amountUsedInTx = new BN(100);
+
+    const txFee = new BN(1);
+
+    const totalCost = amountUsedInTx.add(txFee);
+
+    const canPayResult = canPay(balance, totalCost);
+
+    expect(canPayResult).toStrictEqual(false);
+  });
+});
