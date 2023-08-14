fb8633f13514c1381f349110a107498fcfc49e84
Pablo Labarta <pablitolabarta@gmail.com>
2023-08-14 12:37:33 -0300

    implement initKeyring in validate

--- scripts/cc-cli/src/commands/validate.ts
+++ scripts/cc-cli/src/commands/validate.ts
@@ -1,6 +1,5 @@
 import { Command, OptionValues } from "commander";
 import { newApi } from "../api";
-import { getControllerSeedFromEnvOrPrompt } from "../utils/account";
 import { StakingPalletValidatorPrefs, validate } from "../utils/validate";
 import {
   inputOrDefault,
@@ -8,31 +7,31 @@ import {
   parsePercentAsPerbillOrExit,
 } from "../utils/parsing";
 import { setInteractivity } from "../utils/interactive";
+import { initControllerKeyring } from "../utils/account";
 
 export function makeValidateCommand() {
   const cmd = new Command("validate");
   cmd.description("Signal intention to validate from a Controller account");
   cmd.option(
     "--commission [commission]",
-    "Specify commission for validator in percent",
+    "Specify commission for validator in percent"
   );
   cmd.option(
     "--blocked",
-    "Specify if validator is blocked for new nominations",
+    "Specify if validator is blocked for new nominations"
   );
   cmd.action(validateAction);
   return cmd;
 }
 
 async function validateAction(options: OptionValues) {
-  const interactive = setInteractivity(options);
   const { api } = await newApi(options.url);
 
-  const controllerSeed = await getControllerSeedFromEnvOrPrompt(interactive);
+  const controller = await initControllerKeyring(options);
 
   // Default commission is 0%
   const commission = parsePercentAsPerbillOrExit(
-    inputOrDefault(options.commission, "0"),
+    inputOrDefault(options.commission, "0")
   );
 
   const blocked = parseBoolean(options.blocked);
@@ -41,7 +40,7 @@ async function validateAction(options: OptionValues) {
 
   console.log("Creating validate transaction...");
 
-  const result = await validate(controllerSeed, preferences, api);
+  const result = await validate(controller, preferences, api);
 
   console.log(result.info);
   process.exit(0);
