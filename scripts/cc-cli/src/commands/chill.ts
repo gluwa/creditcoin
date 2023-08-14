01ab8ef96e2dc4e1eacd2901bde2635d38fc99cd
Pablo Labarta <pablitolabarta@gmail.com>
2023-08-14 12:26:45 -0300

    implement initKeyring in chill

--- scripts/cc-cli/src/commands/chill.ts
+++ scripts/cc-cli/src/commands/chill.ts
@@ -1,28 +1,22 @@
 import { Command, OptionValues } from "commander";
 import { newApi } from "../api";
-import {
-  getControllerSeedFromEnvOrPrompt,
-  initKeyringPair,
-} from "../utils/account";
+import { initControllerKeyring } from "../utils/account";
 import { chill } from "../utils/validate";
 import { getValidatorStatus, requireStatus } from "../utils/validatorStatus";
-import { setInteractivity } from "../utils/interactive";
 
 export function makeChillCommand() {
   const cmd = new Command("chill");
   cmd.description(
-    "Signal intention to stop validating from a Controller account",
+    "Signal intention to stop validating from a Controller account"
   );
   cmd.action(chillAction);
   return cmd;
 }
 
 async function chillAction(options: OptionValues) {
-  const interactive = setInteractivity(options);
   const { api } = await newApi(options.url);
 
-  const controllerSeed = await getControllerSeedFromEnvOrPrompt(interactive);
-  const controllerKeyring = initKeyringPair(controllerSeed);
+  const controllerKeyring = await initControllerKeyring(options);
   const controllerAddress = controllerKeyring.address;
 
   const controllerStatus = await getValidatorStatus(controllerAddress, api);
@@ -37,7 +31,7 @@ async function chillAction(options: OptionValues) {
 
   console.log("Creating chill transaction...");
 
-  const result = await chill(controllerSeed, api);
+  const result = await chill(controllerKeyring, api);
 
   console.log(result.info);
   process.exit(0);
