f9d034f17f3c90ebc2fea278d55b08994de1b5f7
Pablo Labarta <pablitolabarta@gmail.com>
2023-08-14 12:24:57 -0300

    implement initKeyring in show-address

--- scripts/cc-cli/src/commands/showAddress.ts
+++ scripts/cc-cli/src/commands/showAddress.ts
@@ -1,26 +1,19 @@
 import { cryptoWaitReady } from "@polkadot/util-crypto";
 import { Command, OptionValues } from "commander";
-import {
-  getCallerSeedFromEnvOrPrompt,
-  initKeyringPair,
-} from "../utils/account";
-import { setInteractivity } from "../utils/interactive";
+import { initCallerKeyring } from "../utils/account";
 
 export function makeShowAddressCommand() {
   const cmd = new Command("show-address");
   cmd.description("Show account address");
+  cmd.option("--ecdsa", "Show address using ECDSA PK");
   cmd.action(showAddressAction);
   return cmd;
 }
 
 async function showAddressAction(options: OptionValues) {
-  const interactive = setInteractivity(options);
   await cryptoWaitReady();
-  const callerSeed = await getCallerSeedFromEnvOrPrompt(interactive);
-  const pair = initKeyringPair(callerSeed);
-  const address = pair.address;
-
+  const caller = await initCallerKeyring(options);
+  const address = caller.address;
   console.log("Account address:", address);
-
   process.exit(0);
 }
