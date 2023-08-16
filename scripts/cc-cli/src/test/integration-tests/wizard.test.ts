c1ef5b504232abe4e17e3f0b1d04b0c9421c5d3e
Pablo Labarta <pablitolabarta@gmail.com>
2023-08-16 16:10:44 -0300

    upgrade wizard test to cover ecdsa/phrase

--- scripts/cc-cli/src/test/integration-tests/wizard.test.ts
+++ scripts/cc-cli/src/test/integration-tests/wizard.test.ts
@@ -1,29 +1,37 @@
 import { newApi } from "../../api";
 import { parseAmountInternal } from "../../utils/parsing";
 import { getValidatorStatus } from "../../utils/validatorStatus";
-import { ALICE_NODE_URL, BOB_NODE_URL, fundAccounts } from "./helpers";
+import { ALICE_NODE_URL, BOB_NODE_URL, fundAddressesFromSudo, randomTestAccount } from "./helpers";
 import execa from "execa";
+import { signSendAndWatch } from "../../utils/tx";
+import { initKeyringPair } from "../../utils/account";
 
 describe("integration test: validator wizard setup", () => {
-  test("new validator should appear as waiting after using the wizard setup", async () => {
+  it.each([
+    ["using an ecdsa pk", true],
+    ["using a seed phrase", false],
+  ])("new validator should appear as waiting after running %s", async (text, ecdsa) => {
     // Fund stash and controller
-    const { stash, controller } = await fundAccounts(
-      parseAmountInternal("10000"),
-    );
+    const stash = randomTestAccount(ecdsa);
+    const controller = randomTestAccount(ecdsa);
+
+    const fundTx = await fundAddressesFromSudo([stash.address, controller.address], parseAmountInternal("10000"));
+    const {api} = await newApi(ALICE_NODE_URL);
+    await signSendAndWatch(fundTx, api, initKeyringPair("//Alice"));
+
     // Run wizard setup with 1k ctc ang to pair with node Bob
     execa.commandSync(
-      `creditcoin-cli wizard --amount 1000 --url ${BOB_NODE_URL}`,
+      `creditcoin-cli wizard --amount 1000 --url ${BOB_NODE_URL} ${ecdsa ? "--ecdsa" : ""}`,
       {
         env: {
-          CC_STASH_SECRET: stash.seed,
-          CC_CONTROLLER_SECRET: controller.seed,
+          CC_STASH_SECRET: stash.secret,
+          CC_CONTROLLER_SECRET: controller.secret,
         },
       },
     );
 
-    const { api } = await newApi(ALICE_NODE_URL);
     const validatorStatus = await getValidatorStatus(stash.address, api);
 
     expect(validatorStatus.waiting).toBe(true);
-  }, 100000);
+  }, 120000);
 });
