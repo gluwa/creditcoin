0ee421e30cfe4a9a069ffd8f8880a9edfd37106f
Alex Todorov <alex.todorov@gluwa.com>
2023-09-04 13:05:17 +0300

    More improvements to unit tests
    - adjust wording
    - add missing scenarios for longer argument length
    - adjust data for scenario with invalid hex chars - must be the correct
      length
    - remove duplicate scenario

--- scripts/cc-cli/src/test/unit-tests/collect-coins.test.ts
+++ scripts/cc-cli/src/test/unit-tests/collect-coins.test.ts
@@ -12,7 +12,7 @@ describe(isTxHashValid, () => {
     ).toBe(true);
   });
 
-  test("Should return false with missing '0x' prefix", () => {
+  test("should return false with missing '0x' prefix", () => {
     expect(
       isTxHashValid(
         "2446f1fd773fbb9f080e674b60c6a033c7ed7427b8b9413cf28a2a4a6da9b56c",
@@ -20,7 +20,7 @@ describe(isTxHashValid, () => {
     ).toBe(false);
   });
 
-  test("Should return false when hash is missing final character", () => {
+  test("should return false when tx hash length < 64 characters", () => {
     expect(
       isTxHashValid(
         "0x2446f1fd773fbb9f080e674b60c6a033c7ed7427b8b9413cf28a2a4a6da9b56",
@@ -28,51 +28,71 @@ describe(isTxHashValid, () => {
     ).toBe(false);
   });
 
-  test("should return false when called with empty", () => {
+  test("should return false when tx hash length > 64 characters", () => {
+    expect(
+      isTxHashValid(
+        "0x2446f1fd773fbb9f080e674b60c6a033c7ed7427b8b9413cf28a2a4a6da9b56ceeeeee",
+      ),
+    ).toBe(false);
+  });
+
+  test("should return false when called with empty string", () => {
     expect(isTxHashValid("")).toBe(false);
   });
 
-  test("Should return false when input has non hexadecimal character", () => {
+  test("Should return false when input has non hexadecimal character(s)", () => {
     expect(
       isTxHashValid(
-        "0x2446f1fd773fbb9f080e674b60c6a033c7ed7427b8b9413cf28a2a4a6da9b56Z",
+        "0x2446f1fd773fbb9f080e674b60-INVALID-d7427b8b9413cf28a2a4a6da9b56c",
       ),
     ).toBe(false);
   });
 });
 
 describe(isExternalAddressValid, () => {
-  test("should return true with proper address", () => {
+  test("should return true with valid address, prefixed with '0x'", () => {
     expect(
       isExternalAddressValid("0x71C7656EC7ab88b098defB751B7401B5f6d8976F"),
     ).toBe(true);
   });
 
-  test("should return true with proper address missing prefix", () => {
+  test("should return true with valid address missing the '0x' prefix", () => {
     expect(
       isExternalAddressValid("71C7656EC7ab88b098defB751B7401B5f6d8976F"),
     ).toBe(true);
-  });
-
-  test("should return false when argument is missing final character", () => {
-    expect(
-      isExternalAddressValid("0x71C7656EC7ab88b098defB751B7401B5f6d8976"),
-    ).toBe(false);
   });
 
   test("Should return false when called with empty string", () => {
     expect(isExternalAddressValid("")).toBe(false);
   });
 
-  test("Should return false when input has non hexadecimal characters", () => {
+  test("should return false when argument length < 42, including the '0x' prefix", () => {
     expect(
-      isExternalAddressValid("0x71C7656EC7ab88b098defB751B7401B5f6d8976Z"),
+      isExternalAddressValid("0x71C7656EC7ab88b098defB751B7401B5f6d8976"),
     ).toBe(false);
   });
 
-  test("Should return false when called with input with length > 42", () => {
+  test("should return false when input has non hexadecimal characters", () => {
+    expect(
+      isExternalAddressValid("0x71C7656EC7ab88b098-INVALID-401B5f6d8976F"),
+    ).toBe(false);
+  });
+
+  test("should return false when argument length > 42, including the '0x' prefix", () => {
     expect(
       isExternalAddressValid("0x71C7656EC7ab88b098defB751B7401B5f6d8976FF"),
+    ).toBe(false);
+  });
+
+  test("should return false when argument length < 40, without the '0x' prefix", () => {
+    expect(
+      isExternalAddressValid("71C7656EC7ab88b098defB751B7401B5f6d8976"),
+    ).toBe(false);
+  });
+
+  test("should return false when argument length > 40, without the '0x' prefix", () => {
+    expect(
+      isExternalAddressValid("71C7656EC7ab88b098defB751B7401B5f6d8976FF"),
     ).toBe(false);
   });
 });
