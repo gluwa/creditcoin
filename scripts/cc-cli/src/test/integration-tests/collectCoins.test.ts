80546d2667765801f3c67de43b4cf1467b5f6bd0
Zachary Frederick <zach.frederick@gluwa.com>
2023-09-19 15:17:50 -0400

    updated imports for cc-cli collectCoins test

--- scripts/cc-cli/src/test/integration-tests/collectCoins.test.ts
+++ scripts/cc-cli/src/test/integration-tests/collectCoins.test.ts
@@ -15,7 +15,7 @@ import { cryptoWaitReady } from "@polkadot/util-crypto";
 import { deployCtcContract, CREDO_PER_CTC } from "creditcoin-js/lib/ctc-deploy";
 import { testData, tryRegisterAddress } from "creditcoin-js/lib/testUtils";
 
-import { forElapsedBlocks } from "creditcoin-js/lib/testUtils/index";
+import { forElapsedBlocks } from "creditcoin-js/lib/testUtils";
 import { describeIf } from "../../utils/tests";
 import { getBalance } from "../../utils/balance";
 
