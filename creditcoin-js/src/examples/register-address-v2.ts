13732467f6f12a0f80bb06e56d66f0d5f9efa67e
Zachary Frederick <zach.frederick@gluwa.com>
2023-09-27 11:10:14 -0400

    formatted creditcoin-js

--- creditcoin-js/src/examples/register-address-v2.ts
+++ creditcoin-js/src/examples/register-address-v2.ts
@@ -3,8 +3,7 @@ import { personalSignAccountId } from '../utils';
 import { createAddressId, personalSignSignature } from '../extrinsics/register-address-v2';
 import { Keyring, creditcoinApi } from '../index';
 
-export async function registerAddressV2Example(
-) {
+export async function registerAddressV2Example() {
     // Create a keyring with Alice
     const creditcoinAddress = new Keyring({
         type: 'sr25519',
@@ -16,7 +15,7 @@ export async function registerAddressV2Example(
     } = await creditcoinApi('ws://127.0.0.1:9944');
 
     const ethSigner = Wallet.createRandom();
-    const blockchain = "Ethereum";
+    const blockchain = 'Ethereum';
 
     const accountId = creditcoinAddress.addressRaw;
     const externalAddress = ethSigner.address;
@@ -27,7 +26,7 @@ export async function registerAddressV2Example(
     const lenderRegAddr = await registerAddressV2(externalAddress, blockchain, proof, creditcoinAddress);
     const addressId = createAddressId(blockchain, ethSigner.address);
 
-    await api.disconnect()
+    await api.disconnect();
 
-    return { lenderRegAddr, addressId }
+    return { lenderRegAddr, addressId };
 }
