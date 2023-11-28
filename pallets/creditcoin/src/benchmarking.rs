a8314dff54272897e6f75a0423d9a6881ef805d7
Alex Todorov <alex.todorov@gluwa.com>
2023-11-28 20:25:12 +0200

    Fix `balance` argument for burn() benchmark to avoid errors
    NOTE: always use a multiple of the existential deposit because
    make_free_balance_be() checks that, see
    https://paritytech.github.io/polkadot-sdk/master/src/pallet_balances/impl_currency.rs.html#476
    
    The reason we were getting the error:
    Error: Input("Benchmark pallet_creditcoin::burn failed: BurnInsufficientFunds")
    
    is that MIN is 500 and the original chosen value of 100 is lower which
    causes the check above to fail.
    
    WARNING: the values `100` and `500` are in CREDO, not in CTC. That is
    10^-18 CTC!

--- pallets/creditcoin/src/benchmarking.rs
+++ pallets/creditcoin/src/benchmarking.rs
@@ -382,8 +382,9 @@ benchmarks! {
 	burn {
 		let collector: T::AccountId = lender_account::<T>(true);
 		let who = collector.clone();
-		<Balances<T> as Currency<T::AccountId>>::make_free_balance_be(&collector, 100u32.into());
-		let balance = 100u32.into();
+		let min = <Balances<T> as Currency<T::AccountId>>::minimum_balance();
+		<Balances<T> as Currency<T::AccountId>>::make_free_balance_be(&collector, min * 1_000_000u32.into());
+		let balance = min * 100u32.into();
 	}: _(RawOrigin::Signed(who), balance, collector)
 }
 
