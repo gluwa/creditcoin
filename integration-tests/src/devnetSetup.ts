106858389bdefde1f96c33ab8dd4ff89ded823f3
Alex Todorov <alex.todorov@gluwa.com>
2024-01-15 16:34:34 +0200

    Update Devnet name value in tests config file

--- integration-tests/src/devnetSetup.ts
+++ integration-tests/src/devnetSetup.ts
@@ -33,8 +33,8 @@ const setup = async () => {
     (global as any).CREDITCOIN_ETHEREUM_USE_HARDHAT_WALLET = false;
 
     (global as any).CREDITCOIN_EXECUTE_SETUP_AUTHORITY = false;
-    (global as any).CREDITCOIN_NETWORK_LONG_NAME = 'Creditcoin Dev';
-    (global as any).CREDITCOIN_NETWORK_SHORT_NAME = 'creditcoin_dev';
+    (global as any).CREDITCOIN_NETWORK_LONG_NAME = 'PoS Devnet';
+    (global as any).CREDITCOIN_NETWORK_SHORT_NAME = 'creditcoin_pos_devnet';
     (global as any).CREDITCOIN_METRICS_BASE = 'http://dev-rpc-creditcoin-rpc-2.centralus.cloudapp.azure.com:9615';
     (global as any).CREDITCOIN_REUSE_EXISTING_ADDRESSES = true;
 
